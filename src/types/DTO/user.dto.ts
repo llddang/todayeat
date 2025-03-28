35c1323aaf27e1c28bad0de721a5ddf338aecadb
llddang <bodyness@naver.com>
2025-03-28 20:44:38 +0900

    feat:#2 auth api 함수 정의 및 type 정의

---
+++ src/types/DTO/user.dto.ts
@@ -0,0 +1,32 @@
+import { Session, User } from '@supabase/supabase-js';
+
+export type UserDTO = {
+  id: string;
+  email: string;
+  name: string;
+  profileImage: string;
+  createdAt: string;
+};
+
+export type UserSnakeCaseDTO = {
+  id: string;
+  email: string;
+  name: string;
+  profile_image: string;
+  created_at: string;
+};
+
+export type UserCreateDTO = Pick<UserDTO, 'email' | 'name'> & {
+  password: string;
+};
+
+export type SupabaseAuthDTO = {
+  user: User | null;
+  session: Session | null;
+};
+
+export type UserAuthDTO = {
+  id: string;
+  email: string;
+  isAuthenticated: boolean;
+};
