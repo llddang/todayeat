c06a47651cb54d716dcacf4eb5ed2b69b5158c54
llddang <bodyness@naver.com>
2025-03-28 21:17:20 +0900

    feat:#2 카카오 로그인 버튼 구현

---
+++ src/components/_auth/auth-sign-in-with-kakao-button.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { signInWithKakao } from '@/libs/utils/apis/auth-browser.api';
+
+const AuthSignInWithKakaoButton = () => {
+  return (
+    <form action={signInWithKakao}>
+      <button>카카오 로그인</button>
+    </form>
+  );
+};
+export default AuthSignInWithKakaoButton;
