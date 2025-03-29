10d787ff90b4e1d3c8f6df27ac559293fba2965a
llddang <bodyness@naver.com>
2025-03-29 19:47:24 +0900

    feat:#4 로그인 버튼 구현

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,29 @@
+import AuthSignInWithGoogleButton from '@/component/_auth/auth-sign-in-with-google-button';
+import AuthSignInWithKakaoButton from '@/component/_auth/auth-sign-in-with-kakao-button';
+import { signIn } from '@/libs/apis/auth-server.api';
+
+const SignInPage = () => {
+  const handleSubmit = async (formData: FormData) => {
+    'use server';
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    if (!email || !password) return;
+
+    await signIn(email, password);
+  };
+
+  return (
+    <section>
+      <form action={handleSubmit}>
+        <label htmlFor="email">이메일</label>
+        <input name="email" type="text" />
+        <label htmlFor="password">비밀번호</label>
+        <input name="password" type="password" />
+        <button type="submit">로그인</button>
+      </form>
+      <AuthSignInWithGoogleButton />
+      <AuthSignInWithKakaoButton />
+    </section>
+  );
+};
+export default SignInPage;
