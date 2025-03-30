065949b0953fdef9954d00e2f763241721a76df4
llddang <bodyness@naver.com>
2025-03-31 01:16:00 +0900

    feat:#4 로그인 폼 구현 및 적용

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,11 @@
+import AuthSignInForm from '@/components/_auth/auth-sign-in-form';
 import AuthSignInWithGoogleButton from '@/components/_auth/auth-sign-in-with-google-button';
 import AuthSignInWithKakaoButton from '@/components/_auth/auth-sign-in-with-kakao-button';
-import { signIn } from '@/libs/apis/auth-server.api';
 
 const SignInPage = () => {
-  const handleSubmit = async (formData: FormData) => {
-    'use server';
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-    if (!email || !password) return;
-
-    await signIn(email, password);
-  };
-
   return (
     <section>
-      <form action={handleSubmit}>
-        <label htmlFor="email">이메일</label>
-        <input name="email" type="text" />
-        <label htmlFor="password">비밀번호</label>
-        <input name="password" type="password" />
-        <button type="submit">로그인</button>
-      </form>
+      <AuthSignInForm />
       <AuthSignInWithGoogleButton />
       <AuthSignInWithKakaoButton />
     </section>
---
+++ src/components/_auth/auth-sign-in-form.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import FormSchema from '@/constants/form-schema.constant';
+import { signIn } from '@/libs/apis/auth-server.api';
+import { UserSignInDTO } from '@/types/DTO/user.dto';
+
+const signInDefaultValue: UserSignInDTO = {
+  email: '',
+  password: ''
+};
+
+const signInSchema = z.object({
+  email: FormSchema.NON_EMPTY_SCHEMA,
+  password: FormSchema.NON_EMPTY_SCHEMA
+});
+
+const AuthSignInForm = () => {
+  const router = useRouter();
+
+  const form = useForm<UserSignInDTO>({
+    mode: 'onBlur',
+    resolver: zodResolver(signInSchema),
+    defaultValues: signInDefaultValue
+  });
+
+  const handleSubmit = async ({ email, password }: UserSignInDTO) => {
+    signIn(email, password)
+      .then(() => router.push('/'))
+      .catch((e) => alert(e.message));
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>이메일</FormLabel>
+              <FormControl>
+                <Input placeholder="example@naver.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="***" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">로그인</Button>
+      </form>
+    </Form>
+  );
+};
+export default AuthSignInForm;
