f7e8d5eb866bb8b461b4effecfdf309a5bb0df6e
llddang <bodyness@naver.com>
2025-03-31 01:17:19 +0900

    design:#4 로그인 페이지의 구성 요소 변경

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,16 @@
 import AuthSignInForm from '@/components/_auth/auth-sign-in-form';
 import AuthSignInWithGoogleButton from '@/components/_auth/auth-sign-in-with-google-button';
 import AuthSignInWithKakaoButton from '@/components/_auth/auth-sign-in-with-kakao-button';
+import Link from 'next/link';
 
 const SignInPage = () => {
   return (
     <section>
+      <h2>로그인</h2>
       <AuthSignInForm />
+      <p>
+        아직 계정이 없으신가요? <Link href="/sign-up">회원가입으로</Link>
+      </p>
       <AuthSignInWithGoogleButton />
       <AuthSignInWithKakaoButton />
     </section>
