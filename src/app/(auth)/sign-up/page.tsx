e4368f06f5a53fc5284fbc5ec476f296ececcffa
llddang <bodyness@naver.com>
2025-03-31 02:14:20 +0900

    design:#4 구성요소 추가

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,14 @@
 import AuthSignUpForm from '@/components/_auth/auth-sign-up-form';
+import Link from 'next/link';
 
 const SignUpPage = () => {
   return (
     <section>
+      <h2>회원가입</h2>
       <AuthSignUpForm />
+      <p>
+        아직 계정이 있으신가요? <Link href="/sign-in">로그인으로</Link>
+      </p>
     </section>
   );
 };
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -6,14 +6,14 @@ import { getAuth } from '@/libs/apis/auth-server.api';
 const Header = async () => {
   const { isAuthenticated } = await getAuth();
   return (
-    <header>
+    <header className="flex justify-between">
       <nav className="space-x-4">
         <Link href={SITE_MAP.HOME}>홈</Link>
         <Link href={SITE_MAP.SIGN_IN}>로그인</Link>
         <Link href={SITE_MAP.SIGN_UP}>회원가입</Link>
       </nav>
       <AuthSignOutButton />
-      {`${isAuthenticated}`}
+      로그인 했니 ? : {`${isAuthenticated}`}
     </header>
   );
 };
