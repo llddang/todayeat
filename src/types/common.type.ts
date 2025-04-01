0471ada58b343e216cf7444940cfab7e632ac161
llddang <bodyness@naver.com>
2025-04-01 10:49:04 +0900

    feat:#15 표기법 변환 유틸 타입 정의

---
+++ src/types/common.type.ts
@@ -0,0 +1,27 @@
+// Camel -> Snake
+export type CamelToSnake<S extends string> = S extends `${infer F}${infer R}`
+  ? F extends Uppercase<F>
+    ? `_${Lowercase<F>}${CamelToSnake<R>}`
+    : `${F}${CamelToSnake<R>}`
+  : S;
+export type SnakeCaseObject<T> =
+  T extends Array<infer U>
+    ? Array<SnakeCaseObject<U>>
+    : T extends object
+      ? { [K in keyof T as CamelToSnake<K & string>]: SnakeCaseObject<T[K]> }
+      : T extends string
+        ? CamelToSnake<T>
+        : T;
+
+// Snake -> Camel
+export type SnakeToCamel<S extends string> = S extends `${infer P}_${infer C}${infer R}`
+  ? `${Lowercase<P>}${Uppercase<C>}${SnakeToCamel<R>}`
+  : Lowercase<S>;
+export type CamelCaseObject<T> =
+  T extends Array<infer U>
+    ? Array<CamelCaseObject<U>>
+    : T extends object
+      ? { [K in keyof T as SnakeToCamel<K & string>]: CamelCaseObject<T[K]> }
+      : T extends string
+        ? SnakeToCamel<T>
+        : T;
