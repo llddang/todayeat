6920f2fa8c5cbec205514d313839017b5ce35909
woohyuckk <woohyuckk@gmail.com>
2025-04-03 20:18:08 +0900

    feat:#31 /meal/post 경로 상수 추가

--- src/constants/site-map.constant.ts
+++ src/constants/site-map.constant.ts
@@ -2,6 +2,7 @@ const SITE_MAP = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
-  CHANGE_PASSWORD: '/change-password'
+  CHANGE_PASSWORD: '/change-password',
+  MEAL_POST: '/meal/post'
 } as const;
 export default SITE_MAP;
