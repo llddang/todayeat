a58f4863c20c406cc124a36238c57dcc1264d7e5
llddang <bodyness@naver.com>
2025-04-03 17:32:40 +0900

    feat:#29 카멜케이스에서 스네이크케이스로 변환하는 유틸 함수 정의

--- src/lib/utils/camelize.util.ts
+++ src/lib/utils/camelize.util.ts
@@ -1,13 +1,24 @@
-import { CamelCaseObject } from '@/types/common.type';
+import { CamelCaseObject, SnakeCaseObject } from '@/types/common.type';
 
-export const toCamelCase = (str: string): string => str.replace(/_([a-z])/g, (_, char: string) => char.toUpperCase());
-export const camelize = <T>(obj: T): CamelCaseObject<T> => {
+/**
+ * snake_case를 camelCase로 변환하는 함수
+ * @param {string} str - 변환할 snake_case 문자열
+ * @returns {string} - 변환된 camelCase 문자열
+ */
+export const snakeToCamel = (str: string): string => str.replace(/_([a-z])/g, (_, char: string) => char.toUpperCase());
+
+/**
+ * 객체의 모든 키를 snake_case에서 camelCase로 재귀적으로 변환
+ * @param {any} obj - 변환할 객체
+ * @returns {any} - 키가 camelCase로 변환된 객체
+ */
+export const snakeToCamelObject = <T>(obj: T): CamelCaseObject<T> => {
   if (obj === null || obj === undefined) {
     return obj as unknown as CamelCaseObject<T>;
   }
 
   if (Array.isArray(obj)) {
-    return obj.map((item) => camelize(item)) as unknown as CamelCaseObject<T>;
+    return obj.map((item) => snakeToCamelObject(item)) as unknown as CamelCaseObject<T>;
   }
 
   if (typeof obj !== 'object') {
@@ -16,10 +27,41 @@ export const camelize = <T>(obj: T): CamelCaseObject<T> => {
 
   const result: Record<string, unknown> = {};
   for (const [key, value] of Object.entries(obj)) {
-    const newKey = toCamelCase(key);
-    if (typeof value === 'object') result[newKey] = camelize(value);
+    const newKey = snakeToCamel(key);
+    if (typeof value === 'object') result[newKey] = snakeToCamelObject(value);
     else result[newKey] = value;
   }
 
   return result as CamelCaseObject<T>;
 };
+
+/**
+ * camelCase를 snake_case로 변환하는 함수
+ * @param {string} str - 변환할 camelCase 문자열
+ * @returns {string} - 변환된 snake_case 문자열
+ */
+export const camelToSnake = (str: string): string => str.replace(/[A-Z]/g, (char) => `_${char.toLowerCase()}`);
+
+/**
+ * 객체의 모든 키를 camelCase에서 snake_case로 재귀적으로 변환
+ * @param {any} obj - 변환할 객체
+ * @returns {any} - 키가 snake_case로 변환된 객체
+ */
+export function camelToSnakeObject<T extends object>(obj: T): SnakeCaseObject<T> {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => camelToSnakeObject(item)) as unknown as SnakeCaseObject<T>;
+  }
+
+  const result: Record<string, unknown> = {};
+  for (const [key, value] of Object.entries(obj)) {
+    const newKey = camelToSnake(key);
+    if (typeof value === 'object') result[newKey] = snakeToCamelObject(value);
+    else result[newKey] = value;
+  }
+
+  return result as SnakeCaseObject<T>;
+}
