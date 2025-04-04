36862d425b02be27bf16ac7c368484cde3403ba7
jinew <jiyunk@naver.com>
2025-04-04 17:12:09 +0900

    add:#30 마이페이지 추가

---
+++ src/app/my-page/page.tsx
@@ -0,0 +1,12 @@
+import MyPageEditProfile from '@/components/_my-page/my-page-edit-profile';
+
+const MyPage = () => {
+  return (
+    <section>
+      <h2>마이페이지</h2>
+      <MyPageEditProfile />
+    </section>
+  );
+};
+
+export default MyPage;
