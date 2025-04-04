75ad7abf0416b9d6f72fe8ad475b7079fa31e182
woohyuckk <woohyuckk@gmail.com>
2025-04-04 18:36:34 +0900

    feat:#31 유틸함수에 'use server'추가

--- src/lib/utils/convert-image-to-base64.util.ts
+++ src/lib/utils/convert-image-to-base64.util.ts
@@ -18,15 +18,24 @@ export const convertImageFileToBase64 = async (file: File): Promise<ImageContent
 };
 
 export const convertImageUrlToBase64 = async (url: string): Promise<ImageContent> => {
-  const response = await fetch(url);
-  const buffer: ArrayBuffer = await response.arrayBuffer();
+  'use server';
+  try {
+    const response = await fetch(url);
 
-  const mimeType = response.headers.get('content-type') || 'image/jpeg';
-
-  return {
-    inlineData: {
-      data: Buffer.from(buffer).toString('base64'),
-      mimeType
+    if (!response.ok) {
+      throw new Error(`이미지 불러오기 실패: ${response.status} ${response.statusText}`);
     }
-  };
+
+    const buffer: ArrayBuffer = await response.arrayBuffer();
+    const mimeType = response.headers.get('content-type') || 'image/jpeg';
+    return {
+      inlineData: {
+        data: Buffer.from(buffer).toString('base64'),
+        mimeType
+      }
+    };
+  } catch (error) {
+    console.error('이미지 인코딩 실패 :', error);
+    throw new Error('이미지 인코딩에 실패하였습니다.');
+  }
 };
