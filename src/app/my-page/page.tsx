12ba9d715d2f5fdb16348f84bf935db7466c6ea0
jinew <jiyunk@naver.com>
2025-04-04 23:27:56 +0900

    design:#36 section 임시 스타일링

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -2,7 +2,7 @@ import MyPageEditProfile from '@/components/_my-page/my-page-edit-profile';
 
 const MyPage = () => {
   return (
-    <section>
+    <section className="relative min-h-[calc(100dvh-48px)] px-[1.25rem]">
       <h2>마이페이지</h2>
       <MyPageEditProfile />
     </section>
