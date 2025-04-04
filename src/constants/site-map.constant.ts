3752b4f66be83ca32baa2657f0d950a39bfed690
llddang <bodyness@naver.com>
2025-04-04 21:23:56 +0900

    feat:#37 sitemap url 추가

--- src/constants/site-map.constant.ts
+++ src/constants/site-map.constant.ts
@@ -2,6 +2,8 @@ const SITE_MAP = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  FIND_PASSWORD: '/find-password',
+  UPDATE_PASSWORD: '/update-password',
   CHANGE_PASSWORD: '/change-password'
 } as const;
 export default SITE_MAP;
