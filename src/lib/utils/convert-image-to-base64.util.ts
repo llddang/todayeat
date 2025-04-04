c58f6100cacd85ad85cb517f44891667ebef1b51
woohyuckk <woohyuckk@gmail.com>
2025-04-04 21:52:20 +0900

    del:#31 url-to-base64 유틸함수 삭제

--- src/lib/utils/convert-image-to-base64.util.ts
+++ src/lib/utils/convert-image-to-base64.util.ts
@@ -16,26 +16,3 @@ export const convertImageFileToBase64 = async (file: File): Promise<ImageContent
     }
   };
 };
-
-export const convertImageUrlToBase64 = async (url: string): Promise<ImageContent> => {
-  'use server';
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`이미지 불러오기 실패: ${response.status} ${response.statusText}`);
-    }
-
-    const buffer: ArrayBuffer = await response.arrayBuffer();
-    const mimeType = response.headers.get('content-type') || 'image/jpeg';
-    return {
-      inlineData: {
-        data: Buffer.from(buffer).toString('base64'),
-        mimeType
-      }
-    };
-  } catch (error) {
-    console.error('이미지 인코딩 실패 :', error);
-    throw new Error('이미지 인코딩에 실패하였습니다.');
-  }
-};
