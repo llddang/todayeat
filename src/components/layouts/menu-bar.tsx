4064fafed8dff28eb3fc8134007a560c9e8019dc
hoki9797 <hoki9797@gmail.com>
2025-04-05 17:23:40 +0900

    feat:#33 메뉴바 레이아웃 대략적인 설정

--- src/components/layouts/menu-bar.tsx
+++ src/components/layouts/menu-bar.tsx
@@ -1,7 +1,13 @@
+import SITE_MAP from '@/constants/site-map.constant';
+import Link from 'next/link';
+
 const MenuBar = () => {
   return (
-    <footer className="fixed bottom-0 flex w-full items-center justify-around bg-yellow-200 py-4">
-      <span>홈</span>
+    <footer className="fixed bottom-0 flex w-full items-center justify-around bg-yellow-100 py-4">
+      {/* TODO - 페이지 명 정해지면 Link로 라우팅 */}
+      <Link href={SITE_MAP.HOME}>
+        <span>홈</span>
+      </Link>
       <span>식사 기록</span>
       <span>리포트</span>
       <span>마이</span>
