dfe2c9c303d97f8cf6c5a734078515c4c9e5674c
hoki9797 <hoki9797@gmail.com>
2025-04-05 16:32:26 +0900

    feat:#33 공통 메뉴바 레이아웃 아웃라인

--- src/components/layouts/menu-bar.tsx
+++ src/components/layouts/menu-bar.tsx
@@ -1,5 +1,12 @@
 const MenuBar = () => {
-  return <div className="fixed bottom-0 w-full bg-yellow-200">MenuBar</div>;
+  return (
+    <footer className="fixed bottom-0 flex w-full items-center justify-around bg-yellow-200 py-4">
+      <span>홈</span>
+      <span>식사 기록</span>
+      <span>리포트</span>
+      <span>마이</span>
+    </footer>
+  );
 };
 
 export default MenuBar;
