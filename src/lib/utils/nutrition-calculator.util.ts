3b7f6b7276a236435f61eea99a02da1fb51aae87
woohyuckk <woohyuckk@gmail.com>
2025-04-05 20:57:43 +0900

    chore:#39 유틸함수 설명 주석 추가

--- src/lib/utils/nutrition-calculator.util.ts
+++ src/lib/utils/nutrition-calculator.util.ts
@@ -4,7 +4,18 @@ import { NutritionResult } from '@/types/nutrition.type';
 
 type UserPhysicalProfileKey = 'gender' | 'height' | 'weight' | 'age' | 'activityLevel' | 'purpose';
 type UserPhysicalProfile = Pick<UserDTO, UserPhysicalProfileKey>;
-
+/**
+ * 사용자의 신체 정보와 목표를 기반으로 하루 권장 섭취 열량과 탄단지 비율을 계산하는 함수입니다.
+ *
+ * @function calculateNutrition
+ * @param {UserPhysicalProfile} - 사용자 성별, 키, 몸무게, 나이, 활동 수준, 운동 목적 정보
+ * @returns {NutritionResult | undefined} NutritionResult 객체 또는 유효하지 않은 입력 시 undefined 반환
+ *
+ * @description
+ * - BMR(기초대사량) 공식을 기반으로 활동계수 및 목적계수를 곱하여 총 권장 섭취 열량을 계산합니다.
+ * - 각 영양소(탄수화물, 단백질, 지방)는 해당 비율과 열량 환산값(탄:4, 단:4, 지:9)을 통해 계산됩니다.
+ * - 성별에 따라 BMR 계산식이 다르게 적용됩니다.
+ */
 export const calculateNutrition = ({
   gender,
   height,
