fc0490c11948b012d2ad9e88640e5c26c3840853
woohyuckk <woohyuckk@gmail.com>
2025-04-05 20:47:43 +0900

    refactor:#39 함수 복잡도 개선 및 타입 분리

--- src/lib/utils/nutrition-calculator.util.ts
+++ src/lib/utils/nutrition-calculator.util.ts
@@ -1,84 +1,38 @@
-import { USER_ACTIVITY_LEVEL } from '@/constants/nutrition.constant';
+import { ACTIVITY_LEVEL, NUTRITION_PURPOSE } from '@/constants/nutrition.constant';
 import { UserDTO } from '@/types/DTO/user.dto';
+import { NutritionResult } from '@/types/nutrition.type';
 
-type userPhysicalProfileKey = 'gender' | 'height' | 'weight' | 'age' | 'activityLevel' | 'purpose';
+type UserPhysicalProfileKey = 'gender' | 'height' | 'weight' | 'age' | 'activityLevel' | 'purpose';
+type UserPhysicalProfile = Pick<UserDTO, UserPhysicalProfileKey>;
 
-type UserPhysicalProfile = Pick<UserDTO, userPhysicalProfileKey>;
+export const calculateNutrition = ({
+  gender,
+  height,
+  weight,
+  age,
+  activityLevel,
+  purpose
+}: UserPhysicalProfile): NutritionResult | undefined => {
+  if (!gender || !height || !weight || !age || !purpose) return;
 
-// type UserNutritionResult = {
-//   dailyCaloriesGoal: number;
-//   dailyCarbohydrate: number;
-//   dailyProtein: number;
-//   dailyFat: number;
-// };
+  const activityFactor = ACTIVITY_LEVEL[activityLevel as keyof typeof ACTIVITY_LEVEL];
+  const { FACTOR, RATIO } = NUTRITION_PURPOSE[purpose as keyof typeof NUTRITION_PURPOSE];
 
-export const calculateNutrition = ({ gender, height, weight, age, activityLevel, purpose }: UserPhysicalProfile) => {
-  if (gender === null || height === null || weight === null || age === null || purpose === null) return;
+  if (!activityFactor) return;
 
-  let activityLevelToNumber: number;
-  let nutritionRatio: number[] = [];
-  let purposeToNumber;
-  let dailyCaloriesGoal;
-  let dailyProtein;
-  let dailyCarbohydrate;
-  let dailyFat;
+  const baseCalories =
+    gender === 'MAN'
+      ? 10 * weight + 6.25 * height - 5 * age + 5
+      : gender === 'WOMAN'
+        ? 10 * weight + 6.25 * height - 5 * age - 161
+        : 0;
 
-  // const activityFactor = USER_ACTIVITY_LEVEL[activityLevel as keyof typeof USER_ACTIVITY_LEVEL]
-  // const purpose
-  switch (activityLevel) {
-    case 'VERY_LOW':
-      activityLevelToNumber = USER_ACTIVITY_LEVEL.VERY_LOW;
-      break;
-    case 'LOW':
-      activityLevelToNumber = USER_ACTIVITY_LEVEL.LOW;
-      break;
-    case 'MODERATE':
-      activityLevelToNumber = USER_ACTIVITY_LEVEL.MODERATE;
-      break;
-    case 'HIGH':
-      activityLevelToNumber = USER_ACTIVITY_LEVEL.HIGH;
-      break;
-    case 'VERY_HIGH':
-      activityLevelToNumber = USER_ACTIVITY_LEVEL.VERY_HIGH;
-      break;
-    default:
-      return;
-  }
+  const dailyCaloriesGoal = Math.floor(baseCalories * activityFactor * FACTOR);
 
-  switch (purpose) {
-    case 'WEIGHT_LOSS':
-      purposeToNumber = 0.8;
-      nutritionRatio = [0.3, 0.4, 0.3];
-      break;
-    case 'WEIGHT_MAINTENANCE':
-      purposeToNumber = 1;
-      nutritionRatio = [0.45, 0.3, 0.25];
-      break;
-    case 'MUSCLE_GAIN':
-      purposeToNumber = 1.1;
-      nutritionRatio = [0.4, 0.4, 0.2];
-      break;
-    default:
-      purposeToNumber = 1;
-  }
-
-  if (gender === 'MAN') {
-    dailyCaloriesGoal = Math.floor(
-      (10 * weight + 6.25 * height - 5 * age + 5) * activityLevelToNumber * purposeToNumber
-    );
-    dailyCarbohydrate = Math.floor((dailyCaloriesGoal * nutritionRatio[0]) / 4);
-    dailyProtein = Math.floor((dailyCaloriesGoal * nutritionRatio[1]) / 4);
-    dailyFat = Math.floor((dailyCaloriesGoal * nutritionRatio[2]) / 9);
-  }
-
-  if (gender === 'WOMAN') {
-    dailyCaloriesGoal = Math.floor(
-      (10 * weight + 6.25 * height - 5 * age - 161) * activityLevelToNumber * purposeToNumber
-    );
-    dailyCarbohydrate = Math.floor((dailyCaloriesGoal * nutritionRatio[0]) / 4);
-    dailyProtein = Math.floor((dailyCaloriesGoal * nutritionRatio[1]) / 4);
-    dailyFat = Math.floor((dailyCaloriesGoal * nutritionRatio[2]) / 9);
-  }
+  const [carbRatio, proteinRatio, fatRatio] = RATIO;
+  const dailyCarbohydrate = Math.floor((dailyCaloriesGoal * carbRatio) / 4);
+  const dailyProtein = Math.floor((dailyCaloriesGoal * proteinRatio) / 4);
+  const dailyFat = Math.floor((dailyCaloriesGoal * fatRatio) / 9);
 
   return {
     dailyCaloriesGoal,
