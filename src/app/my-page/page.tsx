6870918170dfbd8f458233bbfc571a15df64b8f8
jinew <jiyunk@naver.com>
2025-04-06 18:42:45 +0900

    refactor:#36 유저 정보를 props로 내려주도록 변경

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,10 +1,13 @@
 import MyPageEditProfile from '@/components/_my-page/my-page-edit-profile';
+import { getUser } from '@/lib/apis/user.api';
 
-const MyPage = () => {
+const MyPage = async () => {
+  const userInfo = await getUser();
+
   return (
     <section className="relative min-h-[calc(100dvh-48px)] px-[1.25rem]">
       <h2>마이페이지</h2>
-      <MyPageEditProfile />
+      <MyPageEditProfile userInfo={userInfo} />
     </section>
   );
 };
