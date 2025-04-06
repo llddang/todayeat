4722e2309b3401e43b575fdc583a8a74efb9f722
hoki9797 <hoki9797@gmail.com>
2025-04-06 18:09:53 +0900

    fix: useSearchParams 빌드 에러 수정

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import BackButton from './header-backbutton';
 import SITE_MAP from '@/constants/site-map.constant';
+import { Suspense } from 'react';
 
 const Header = async () => {
   return (
     <header className="relative flex justify-center bg-yellow-100 p-4">
-      <BackButton />
+      <Suspense>
+        <BackButton />
+      </Suspense>
       <Link href={SITE_MAP.HOME} className="text-lg font-bold">
         투데잇
       </Link>
