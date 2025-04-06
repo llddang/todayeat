b3574eba2377b551171c832f444a69957cbd2507
llddang <bodyness@naver.com>
2025-04-06 14:29:21 +0900

    feat:#44 query-string의 값과 현재 스텝의 값이 동일하도록 수정

--- src/lib/hooks/use-funnel.tsx
+++ src/lib/hooks/use-funnel.tsx
@@ -1,9 +1,26 @@
 'use client';
 
 import { ReactNode, useState, ReactElement } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+const QUERY_PARAM = 'step';
 
 const useFunnel = <T extends string>(initialStep: T) => {
-  const [step, setStep] = useState<T>(initialStep);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const urlStep = searchParams.get(QUERY_PARAM) as T;
+  const [step, setInternalStep] = useState<T>(urlStep || initialStep);
+
+  const setStep = (newStep: T) => {
+    if (step === newStep) return;
+    setInternalStep(newStep);
+
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set(QUERY_PARAM, newStep);
+    router.push(`${pathname}?${newSearchParams.toString()}`, { scroll: false });
+  };
 
   type StepProps = {
     name: T;
