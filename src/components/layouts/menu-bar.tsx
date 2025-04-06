6befb1b996ef2c8458ff15d7e7271809eea6b3c1
hoki9797 <hoki9797@gmail.com>
2025-04-06 17:53:04 +0900

    feat:#33 시멘틱 태그 변경 및 해당 페이지에서 강조 표시

--- src/components/layouts/menu-bar.tsx
+++ src/components/layouts/menu-bar.tsx
@@ -1,17 +1,40 @@
+'use client';
+
 import SITE_MAP from '@/constants/site-map.constant';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const MenuBar = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname === path;
+  const emphasizeClass = (path: string) => (isActive(path) ? 'font-bold text-xl' : '');
+
   return (
-    <footer className="fixed bottom-0 flex w-full items-center justify-around bg-yellow-100 py-4">
-      {/* TODO - 페이지 명 정해지면 Link로 라우팅 */}
-      <Link href={SITE_MAP.HOME}>
-        <span>홈</span>
-      </Link>
-      <span>식사 기록</span>
-      <span>리포트</span>
-      <span>마이</span>
-    </footer>
+    <nav className="fixed bottom-0 w-full bg-yellow-100 py-4">
+      <ul className="flex items-center justify-around text-sm">
+        <li>
+          <Link href={SITE_MAP.HOME}>
+            <span className={emphasizeClass(SITE_MAP.HOME)}>홈</span>
+          </Link>
+        </li>
+        <li>
+          <Link href={SITE_MAP.MEAL_POST}>
+            <span className={emphasizeClass(SITE_MAP.MEAL_POST)}>식단 기록</span>
+          </Link>
+        </li>
+        <li>
+          <Link href={SITE_MAP.REPORT}>
+            <span className={emphasizeClass(SITE_MAP.REPORT)}>리포트</span>
+          </Link>
+        </li>
+        <li>
+          <Link href={SITE_MAP.MY_PAGE}>
+            <span className={emphasizeClass(SITE_MAP.MY_PAGE)}>마이</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
