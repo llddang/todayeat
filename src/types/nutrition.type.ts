e78840bb340f3bcd654b64b3b33a26bfe2234739
woohyuckk <woohyuckk@gmail.com>
2025-04-07 15:10:12 +0900

    refactor:#39 활동계수, 식단목적 타입컨벤션에 맞도록 수정

--- src/types/nutrition.type.ts
+++ src/types/nutrition.type.ts
@@ -1,21 +1,10 @@
-export type ActivityLevelKey = 'VERY_LOW' | 'LOW' | 'MODERATE' | 'HIGH' | 'VERY_HIGH';
-
-export type ActivityLevel = Record<ActivityLevelKey, number>;
-
-export type NutritionPurposeKey = 'WEIGHT_LOSS' | 'WEIGHT_MAINTENANCE' | 'MUSCLE_GAIN';
-
-export type NutritionPurposeValue = {
-  FACTOR: number;
-  RATIO: [number, number, number]; // [탄수화물, 단백질, 지방]
-};
-
-export type NutritionPurpose = Record<NutritionPurposeKey, NutritionPurposeValue>;
+import { MealDTO } from '@/types/DTO/meal.dto';
 
 export type NutritionGoal = {
-  dailyCaloriesGoal: number | null;
-  dailyCarbohydrateGoal: number | null;
-  dailyProteinGoal: number | null;
-  dailyFatGoal: number | null;
+  dailyCaloriesGoal: number;
+  dailyCarbohydrateGoal: number;
+  dailyProteinGoal: number;
+  dailyFatGoal: number;
 };
 
 export type AverageNutrition = {
@@ -31,3 +20,78 @@ export type NutritionRatio = {
   proteinRatio: number;
   fatRatio: number;
 };
+
+export type MealNutrition = Pick<MealDTO, 'totalCalories' | 'totalCarbohydrate' | 'totalFat' | 'totalProtein'>;
+
+/**
+ * 활동 수준별 활동 계수를 정의한 상수 객체입니다.
+ *
+ * @description
+ * - 활동 수준(activity level)에 따라 일일 칼로리 소비량 계산에 사용됩니다.
+ * - 예: VERY_LOW → 거의 움직이지 않음, HIGH → 주 6~7일 고강도 운동 등
+ */
+
+// ActivityLevel의 enum 타입 객체
+export const ActivityLevel = {
+  VERY_LOW: 'VERY_LOW',
+  LOW: 'LOW',
+  MODERATE: 'MODERATE',
+  HIGH: 'HIGH',
+  VERY_HIGH: 'VERY_HIGH'
+} as const;
+
+export type ActivityLevelKey = keyof typeof ActivityLevel;
+
+// ActivityLevel의 enum 값을 통해 사용하는 곳에서 호출되는 객체에 대한 설명
+export const ACTIVITY_LEVEL_OPTIONS: Record<ActivityLevelKey, { description: string; factor: number }> = {
+  VERY_LOW: { description: '거의 움직이지 않아요', factor: 1.2 },
+  LOW: { description: '아주 가볍게 활동해요', factor: 1.375 },
+  MODERATE: { description: '일상적으로 움직여요', factor: 1.55 },
+  HIGH: { description: '자주 활동하거나 운동해요', factor: 1.725 },
+  VERY_HIGH: { description: '하루에 여러 번 강도 높은 운동을 해요', factor: 1.9 }
+} as const;
+
+export const NutritionPurpose = {
+  WEIGHT_LOSS: 'WEIGHT_LOSS',
+  WEIGHT_MAINTENANCE: 'WEIGHT_MAINTENANCE',
+  MUSCLE_GAIN: 'MUSCLE_GAIN'
+};
+
+export const NUTRITION_PURPOSE_OPTIONS = {
+  WEIGHT_LOSS: {
+    name: '체지방 줄이기',
+    factor: 0.8,
+    ratio: {
+      carbRatio: 0.3,
+      proteinRatio: 0.4,
+      fatRatio: 0.3
+    }
+  },
+  WEIGHT_MAINTENANCE: {
+    name: '현재 상태 유지하기',
+    factor: 1,
+    ratio: {
+      carbRatio: 0.45,
+      proteinRatio: 0.3,
+      fatRatio: 0.25
+    }
+  },
+  MUSCLE_GAIN: {
+    name: '근육 키우기',
+    factor: 1.1,
+    ratio: {
+      carbRatio: 0.4,
+      proteinRatio: 0.4,
+      fatRatio: 0.2
+    }
+  }
+} as const;
+
+export type NutritionPurposeKey = keyof typeof NUTRITION_PURPOSE_OPTIONS;
+export type NutritionPurposeRatio = Record<'carbRatio' | 'proteinRatio' | 'fatRatio', number>;
+export type NutritionPurposeValue = {
+  name: string;
+  factor: number;
+  ratio: NutritionPurposeRatio;
+};
+export type NutritionPurpose = Record<NutritionPurposeKey, NutritionPurposeValue>;
