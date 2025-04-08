63dc615b3145d164ecd6d5819a3a3bcc284a5be1
jinew <jiyunk@naver.com>
2025-04-08 19:29:32 +0900

    refactor:#49 페이지네이션 분리

--- src/components/_meal/meal-images-carousel.tsx
+++ src/components/_meal/meal-images-carousel.tsx
@@ -17,15 +17,20 @@ const MealImageCarousel = ({ imageList, pagination = true }: { imageList: string
     setCount(api.scrollSnapList().length);
     setCurrent(api.selectedScrollSnap());
 
-    api.on('select', () => {
+    const onSelectHandler = () => {
       setCurrent(api.selectedScrollSnap());
-    });
+    };
+
+    api.on('select', onSelectHandler);
+
+    return () => {
+      api.off('select', onSelectHandler);
+    };
   }, [api]);
 
   const handleClickPagination = (index: number) => {
     if (api) api.scrollTo(index);
   };
-
   // 페이지네이션이 사용 가능한 경우 - api 로드 완료 && 페이지네이션 사용 true && 이미지 1개 이상
   const isPaginationOn = api && pagination && count > 1;
 
@@ -33,28 +38,40 @@ const MealImageCarousel = ({ imageList, pagination = true }: { imageList: string
     <Carousel setApi={setApi} className="w-full overflow-hidden rounded-2xl">
       <CarouselContent className="ml-0 gap-4">
         {imageList.map((imageUrl, index) => (
-          <CarouselItem
-            key={imageUrl}
-            className="relative aspect-square w-full overflow-hidden rounded-2xl bg-black pl-0"
-          >
+          <CarouselItem key={imageUrl} className="relative aspect-square w-full overflow-hidden rounded-2xl pl-0">
             <Image src={imageUrl} alt={`식단 기록 이미지 ${index + 1}`} fill priority className="object-cover" />
           </CarouselItem>
         ))}
       </CarouselContent>
 
-      {isPaginationOn && (
-        <div className="carousel-bottom-area absolute bottom-[0.6rem] left-1/2 flex -translate-x-1/2 gap-2 rounded-[3.125rem] bg-[#BFBFBF44] px-[0.75rem] py-[0.5rem] backdrop-blur-xl">
-          {Array.from({ length: count }, (_, index) => (
-            <button
-              key={index}
-              className={`inline-block h-2 w-2 rounded-full bg-primary ${current === index ? 'opacity-1' : 'opacity-30'}`}
-              onClick={() => handleClickPagination(index)}
-            ></button>
-          ))}
-        </div>
-      )}
+      {isPaginationOn && <Pagination current={current} count={count} onClick={handleClickPagination} />}
     </Carousel>
   );
 };
 
 export default MealImageCarousel;
+
+// 페이지네이션 컴포넌트
+const Pagination = ({
+  current,
+  count,
+  onClick
+}: {
+  current: number;
+  count: number;
+  onClick: (index: number) => void;
+}) => {
+  return (
+    <nav className="carousel-bottom-area absolute bottom-[0.6rem] left-1/2 flex -translate-x-1/2 gap-2 rounded-[3.125rem] bg-[#BFBFBF44] px-[0.75rem] py-[0.5rem] backdrop-blur-xl">
+      {Array.from({ length: count }, (_, index) => (
+        <button
+          key={index}
+          className={`inline-block h-2 w-2 rounded-full bg-primary ${current === index ? 'opacity-1' : 'opacity-30'}`}
+          onClick={() => onClick(index)}
+          aria-label={`${index + 1}번 이미지로 이동`}
+          aria-current={current === index ? 'true' : 'false'}
+        ></button>
+      ))}
+    </nav>
+  );
+};
