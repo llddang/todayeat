85a5d6886fa11412e9b0397839f8dc05530614a7
hoki9797 <hoki9797@gmail.com>
2025-04-09 14:13:07 +0900

    feat:#58 props 타입 명시

--- src/components/layouts/gradient-blur-background.tsx
+++ src/components/layouts/gradient-blur-background.tsx
@@ -1,4 +1,8 @@
-const GradientBlurBackground = ({ children }: { children: React.ReactNode }) => {
+type GradientBlurBackgroundProps = {
+  children: React.ReactNode;
+};
+
+const GradientBlurBackground = ({ children }: GradientBlurBackgroundProps) => {
   return (
     <div className="relative min-h-screen overflow-hidden">
       <div className="bg-gradient-blur absolute inset-0 -z-10 h-full w-full" />
