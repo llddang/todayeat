7eb656b4c345a2886e2b1557c99e3bf03fba7c88
jinew <jiyunk@naver.com>
2025-04-09 21:36:08 +0900

    refactor:#56 상수에 readonly 및 upper snake case 적용

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -2,15 +2,14 @@
 export const FUNNEL_QUERY_PARAM = 'step';
 
 const MACRO_NAME_MAP: Record<string, string> = {
-  carbohydrate: '탄수화물',
-  protein: '단백질',
-  fat: '지방'
-};
+  CARBOHYDRATE: '탄수화물',
+  PROTEIN: '단백질',
+  FAT: '지방'
+} as const;
 
 // TODO config 설정 시 컬러 변경 필요
 export const MACRO_COLOR_MAP: Record<string, string> = {
-  [MACRO_NAME_MAP.carbohydrate]: 'bg-[#E3C0FA]',
-  [MACRO_NAME_MAP.protein]: 'bg-[#9BE7D6]',
-  [MACRO_NAME_MAP.fat]: 'bg-[#B8E7FF]'
-};
-
+  [MACRO_NAME_MAP.CARBOHYDRATE]: 'bg-[#E3C0FA]',
+  [MACRO_NAME_MAP.PROTEIN]: 'bg-[#9BE7D6]',
+  [MACRO_NAME_MAP.FAT]: 'bg-[#B8E7FF]'
+} as const;
