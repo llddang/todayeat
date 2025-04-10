6e57e2953097050384313591a9d937bbe544825a
jinew <jiyunk@naver.com>
2025-04-10 11:22:54 +0900

    add:#56 일일 칼로리 요약 컴포넌트 추가

---
+++ src/components/_home/calories-summary-card.tsx
@@ -0,0 +1,50 @@
+import { formatNumberWithComma } from '@/lib/utils/format-number-with-comma';
+import { getPercentage } from '@/lib/utils/nutrition-calculator.util';
+
+type CalorieSummaryCardProps = {
+  total: number;
+  goal: number;
+};
+
+const CaloriesSummaryCard = ({ total, goal }: CalorieSummaryCardProps) => {
+  const RADIUS = 44;
+  const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+  const percentage = Math.min(getPercentage(total, goal), 100);
+  const offset = CIRCUMFERENCE - (CIRCUMFERENCE * percentage) / 100;
+
+  const handlefeedbackMessage = () => {
+    if (total < goal) return `${goal - total}kcal 더 먹을 수 있어요`;
+    else return `${total - goal}kcal 더 먹었어요`;
+  };
+
+  return (
+    <div className="mb-3 flex items-center justify-between rounded-2xl bg-white p-4">
+      <div>
+        <h3 className="text-gray-550 mb-1 text-[13px]">총 섭취 칼로리</h3>
+        <span className="mb-2 block text-4xl font-bold">{formatNumberWithComma(total)}</span>
+        <p className="text-gray-550 mb-2 text-sm">
+          1일 목표 칼로리<span className="ml-1 font-semibold">{formatNumberWithComma(goal)}</span>
+        </p>
+        <p className="text-sm font-medium">{handlefeedbackMessage()}</p>
+      </div>
+      <div className="relative h-[6.25rem] w-[6.25rem]">
+        <svg className="rotate-90 transform" viewBox="0 0 100 100">
+          <circle cx="50" cy="50" r={RADIUS} stroke="#f3f3f3" strokeWidth="10" fill="none" />
+          <circle
+            cx="50"
+            cy="50"
+            r={RADIUS}
+            stroke="#FFE37E"
+            strokeWidth="10"
+            fill="none"
+            strokeDasharray={CIRCUMFERENCE}
+            strokeDashoffset={offset}
+            strokeLinecap="round"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default CaloriesSummaryCard;
