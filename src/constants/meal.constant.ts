06686a8e94515bd89722c471a4b278adf13d7b16
llddang <bodyness@naver.com>
2025-04-10 14:08:18 +0900

    move:#61 일러스트 폴더/파일명 변경

--- src/constants/meal.constant.ts
+++ src/constants/meal.constant.ts
@@ -1,9 +1,9 @@
 import { StaticImageData } from 'next/image';
 import { MealCategoryType } from '@/types/meal-category.type';
-import BREAKFAST from '@/../public/meal-category/breakfast.svg';
-import LUNCH from '@/../public/meal-category/lunch.svg';
-import DINNER from '@/../public/meal-category/dinner.svg';
-import SNACK from '@/../public/meal-category/snack.svg';
+import BREAKFAST from '@/../public/illustrations/meal-category-breakfast.svg';
+import LUNCH from '@/../public/illustrations/meal-category-lunch.svg';
+import DINNER from '@/../public/illustrations/meal-category-dinner.svg';
+import SNACK from '@/../public/illustrations/meal-category-snack.svg';
 
 export const MEAL_CATEGORY_OPTIONS: { [key in MealCategoryType]: { name: string; icon: StaticImageData } } = {
   BREAKFAST: { name: '아침', icon: BREAKFAST },
