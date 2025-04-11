b29b697d94000c5b31e1f634f7b5995c63806371
llddang <bodyness@naver.com>
2025-04-11 21:45:15 +0900

    design:#78 button에 padding-y 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-1 whitespace-nowrap text-sm font-semibold tracking-[-0.02em] leading-[140%] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none',
+  'py-[0.625rem] inline-flex items-center justify-center gap-1 whitespace-nowrap text-sm font-semibold tracking-[-0.02em] leading-[140%] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none',
   {
     variants: {
       variant: {
