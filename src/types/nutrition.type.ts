ece6155610ce7a664df024f103e21799da5479ce
woohyuckk <woohyuckk@gmail.com>
2025-04-11 11:43:50 +0900

    feat:#59 Macronutrient, MeasureMentUnit 타입 추가

--- src/types/nutrition.type.ts
+++ src/types/nutrition.type.ts
@@ -8,6 +8,7 @@ export type NutritionGoal = {
 };
 
 export type MealNutrition = Pick<MealDetailDTO, 'calories' | 'carbohydrate' | 'protein' | 'fat'>;
+export type MealNutritionType = keyof MealNutrition;
 export type Macronutrient = Omit<MealNutrition, 'calories'>;
 
 export type PurposeValue = {
@@ -15,3 +16,56 @@ export type PurposeValue = {
   factor: number;
   ratio: Macronutrient;
 };
+
+export const MacronutrientEnum = {
+  CALORIES: 'CALORIES',
+  CARBOHYDRATE: 'CARBOHYDRATE',
+  PROTEIN: 'PROTEIN',
+  FAT: 'FAT'
+} as const;
+
+export const MacronutrientOptions = {
+  CALORIES: {
+    label: '칼로리',
+    unit: 'kcal',
+    color: ''
+  },
+  CARBOHYDRATE: {
+    label: '탄수화물',
+    unit: 'g',
+    color: 'bg-purple-100'
+  },
+  PROTEIN: {
+    label: '단백질',
+    unit: 'g',
+    color: 'bg-teal-100'
+  },
+  FAT: {
+    label: '지방',
+    unit: 'g',
+    color: 'bg-blue-75'
+  }
+} as const;
+
+export type MacronutrientType = keyof typeof MacronutrientOptions;
+export type MacronutrientValues = (typeof MacronutrientOptions)[MacronutrientType];
+export type MacronutrientLabelValue = (typeof MacronutrientOptions)[MacronutrientType]['label'];
+
+export const MeasurementUnitEnum = {
+  KCAL: 'KCAL',
+  GRAM: 'GRAM'
+} as const;
+
+export const MeasurementUnit = {
+  KCAL: {
+    label: '칼로리',
+    unit: 'kcal'
+  },
+  GRAM: {
+    label: '그램',
+    unit: 'g'
+  }
+} as const;
+
+export type MeasurementUnitType = keyof typeof MeasurementUnit;
+export type MeasurementUnitLabelValue = (typeof MeasurementUnit)[MeasurementUnitType]['label'];
