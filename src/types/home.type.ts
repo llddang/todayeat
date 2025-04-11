09d39917de027d71505a71903d2892c80e8d264b
jinew <jiyunk@naver.com>
2025-04-11 16:21:33 +0900

    chore:#75 타입명 구체화

--- src/types/home.type.ts
+++ src/types/home.type.ts
@@ -2,7 +2,7 @@ import { MacronutrientEnumType } from '@/types/nutrition.type';
 
 export type StatusType = 'low' | 'high';
 
-export type DiffItem = {
+export type MacronutrientDiffItem = {
   name: MacronutrientEnumType;
   diff: number;
   label: string;
