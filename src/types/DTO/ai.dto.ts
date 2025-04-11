c2b25b50b4266a51db078a2afe006d1fbd69e648
hoki9797 <hoki9797@gmail.com>
2025-04-11 22:20:32 +0900

    feat:#69 DTO 타입 설정

--- src/types/DTO/ai.dto.ts
+++ src/types/DTO/ai.dto.ts
@@ -1,8 +1,5 @@
-import { SnakeCaseObject } from '@/types/common.type';
 import { FoodAnalysisResult } from '@/types/gemini.type';
 
 export type FoodAnalysisRequestsDetailDTO = FoodAnalysisResult & {
   requestId: string;
 };
-
-export type FoodAnalysisRequestsDetailSnakeCaseDTO = SnakeCaseObject<FoodAnalysisRequestsDetailDTO>;
