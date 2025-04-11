bb4b617021125d1c33bbd0e92b3f937ca2124c70
llddang <bodyness@naver.com>
2025-04-12 00:09:16 +0900

    feat:#78 typography 반영

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'py-[0.625rem] inline-flex items-center justify-center gap-1 whitespace-nowrap text-sm font-semibold tracking-[-0.02em] leading-[140%] transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none',
+  'py-[0.625rem] inline-flex items-center justify-center gap-1 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none',
   {
     variants: {
       variant: {
@@ -17,9 +17,9 @@ const buttonVariants = cva(
         icon: 'text-gray-600 hover:text-gray-800 disabled:text-gray-400 after:bg-no-repeat after:bg-center after:bg-right-line-gray-600-icon hover:after:bg-right-line-gray-800-icon disabled:after:bg-right-line-gray-400-icon'
       },
       size: {
-        default: 'h-11',
-        sm: 'h-10 text-sm gap-[0.125rem] after:w-4 after:h-4',
-        lg: 'h-11 text-base gap-1 after:w-[1.125rem] after:h-[1.125rem]',
+        default: 'h-11 typography-subTitle4 ',
+        sm: 'h-10 typography-subTitle4 gap-[0.125rem] after:w-4 after:h-4',
+        lg: 'h-11 typography-subTitle2 gap-1 after:w-[1.125rem] after:h-[1.125rem]',
         icon: 'h-9 w-9'
       }
     },
