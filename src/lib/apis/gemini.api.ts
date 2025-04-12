7fc41a80bf9c07534393268646edbded06b36dcb
hoki9797 <hoki9797@gmail.com>
2025-04-12 20:03:49 +0900

    refactor:#69 함수 컨벤션 유지 및 반환 타입 명시

--- src/lib/apis/gemini.api.ts
+++ src/lib/apis/gemini.api.ts
@@ -2,11 +2,11 @@ import { FOOD_IMAGE_ANALYSIS_PROMPT } from '@/constants/prompt.constant';
 import { generativeAI } from '@/lib/utils/gemini-instance.util';
 import { ImageContent } from '@/types/gemini.type';
 
-export async function generateFoodAnalysisByImage(imageParts: ImageContent[]) {
+export const generateFoodAnalysisByImage = async (imageParts: ImageContent[]): Promise<string> => {
   const model = generativeAI.getGenerativeModel({
     model: 'gemini-2.0-flash'
   });
 
   const result = await model.generateContent([...imageParts, FOOD_IMAGE_ANALYSIS_PROMPT]);
   return result.response.text();
-}
+};
