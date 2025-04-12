f28c31ae5cdc2b9ba1453bd0c60adbccabea9050
woohyuckk <woohyuckk@gmail.com>
2025-04-12 19:04:21 +0900

    refactor:#59 타이포그래피 적용

--- src/components/_meal/_post/meal-post-edit-nutrient-box.tsx
+++ src/components/_meal/_post/meal-post-edit-nutrient-box.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@/components/ui/typography';
 import { MACRONUTRIENT_OPTIONS } from '@/constants/nutrition.constant';
 import { MacronutrientType } from '@/types/nutrition.type';
 
@@ -9,16 +10,18 @@ type MealEditNutrientBoxProps = {
 const MealEditNutrientBox = ({ variety, value }: MealEditNutrientBoxProps) => {
   const { label, unit, color } = MACRONUTRIENT_OPTIONS[variety];
   return (
-    <div className="flex shrink-0 flex-grow items-start gap-[0.375rem]">
-      <div className="flex h-5 items-center justify-center gap-2">
-        <div className={`h-2 w-2 rounded-full ${color}`} />
+    <div className="flex flex-1 items-start gap-1.5">
+      <div className="flex h-5 items-center justify-center">
+        <div className={`aspect-square h-2 w-2 rounded-full ${color}`} />
       </div>
-      <div className="flex shrink-0 flex-grow flex-col items-start gap-[2px]">
-        <span className="tracking-sung text-[0.875rem]/[20px] font-normal text-gray-700">{label}</span>
-        <span className="self-stretch text-[0.875rem]/[20px] font-semibold tracking-snug">
+      <div className="flex flex-1 flex-col items-start gap-1.5">
+        <Typography as="span" variant="body3" className="text-gray-700">
+          {label}
+        </Typography>
+        <Typography as="span" variant="subTitle4" className="text-gray-900">
           {value}
           {unit}
-        </span>
+        </Typography>
       </div>
     </div>
   );
