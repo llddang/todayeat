987aa9d95134bc951884e737fcc435f0a1f0f34f
jinew <jiyunk@naver.com>
2025-04-12 17:30:29 +0900

    refactor:#80 불필요한 cva 삭제

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,23 +1,21 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
-import { cva } from 'class-variance-authority';
 import { useFormField } from '@/components/ui/form';
 
 type InputProps = React.ComponentProps<'input'> & {
   measure?: string;
 };
 
-const InputVariants = cva(
-  'flex h-12 items-center gap-2 rounded-lg border-[1px] border-gray-200 bg-white px-4 focus-within:!border-gray-700 hover:border-gray-500 '
-);
+const InputVariants =
+  'flex h-12 items-center gap-2 rounded-lg border-[1px] border-gray-200 bg-white px-4 focus-within:!border-gray-700 hover:border-gray-500';
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type = 'text', measure, ...props }, ref) => {
   const { error } = useFormField();
   const isDisabled = props.disabled;
 
   return (
-    <div className={cn(InputVariants(), error && 'border-red-500', isDisabled && 'hover:border-gray-200', className)}>
+    <div className={cn(InputVariants, error && 'border-red-500', isDisabled && 'hover:border-gray-200', className)}>
       <input
         {...props}
         ref={ref}
