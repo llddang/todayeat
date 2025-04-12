fd76ecdf6d43a913d67c2c932eb88b769723935a
hoki9797 <hoki9797@gmail.com>
2025-04-12 17:38:36 +0900

    chore:#92 시멘틱 태그 변경'

--- src/components/commons/footer.tsx
+++ src/components/commons/footer.tsx
@@ -2,11 +2,11 @@ import { Typography } from '@/components/ui/typography';
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center px-4 py-7">
+    <footer className="flex items-center justify-center px-4 py-7">
       <Typography as="span" variant="body3" className="text-gray-400">
         Copyright 2025. 투데잇 All rights reserved.
       </Typography>
-    </div>
+    </footer>
   );
 };
 export default Footer;
