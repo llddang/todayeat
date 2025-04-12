586c431a84b3a9a0de471f3f389822f2c03cfb95
llddang <bodyness@naver.com>
2025-04-12 13:57:43 +0900

    feat:#93 button link 컴포넌트 디자인 변경

--- src/components/commons/button-link.tsx
+++ src/components/commons/button-link.tsx
@@ -10,12 +10,35 @@ const buttonLinkVariants = cva(
   {
     variants: {
       variant: {
-        primary:
-          'rounded-[3.125rem] px-6 bg-gray-900 text-gray-50 hover:bg-gray-800 disabled:text-gray-500 disabled:bg-opacity-5',
+        primary: 'rounded-[3.125rem] px-6 bg-gray-900 text-gray-50 hover:bg-gray-800',
         secondary:
-          'rounded-[3.125rem] px-6 bg-white border border-solid border-gray-350/40 hover:border-purple-10 hover:bg-button-gradient disabled:border-gray-200 disabled:bg-white disabled:text-gray-500',
-        ghost: 'rounded-[3.125rem] px-6 text-gray-600 hover:text-gray-800 disabled:text-gray-400',
-        icon: 'text-gray-600 hover:text-gray-800 disabled:text-gray-400 after:bg-no-repeat after:bg-center after:bg-right-line-gray-600-icon hover:after:bg-right-line-gray-800-icon disabled:after:bg-right-line-gray-400-icon'
+          'rounded-[3.125rem] px-6 text-gray-900 bg-white border border-solid border-gray-350/40 hover:border-purple-10 hover:bg-button-gradient',
+        ghost: 'rounded-[3.125rem] px-6 text-gray-600 hover:text-gray-800 ',
+        icon: 'text-gray-600 hover:text-gray-800 after:bg-no-repeat after:bg-center after:bg-right-line-gray-600-icon hover:after:bg-right-line-gray-800-icon'
+      },
+      size: {
+        default: 'h-11 typography-subTitle4 ',
+        sm: 'h-10 typography-subTitle4 gap-[0.125rem] after:w-4 after:h-4',
+        lg: 'h-11 typography-subTitle2 gap-1 after:w-[1.125rem] after:h-[1.125rem]',
+        icon: 'h-9 w-9'
+      }
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'default'
+    }
+  }
+);
+
+const buttonLinkDisabledVariants = cva(
+  'py-[0.625rem] inline-flex items-center justify-center gap-1 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
+  {
+    variants: {
+      variant: {
+        primary: 'rounded-[3.125rem] px-6 bg-gray-900/5 text-gray-500',
+        secondary: 'rounded-[3.125rem] px-6 bg-white border border-solid border-gray-200 text-gray-500',
+        ghost: 'rounded-[3.125rem] px-6 text-gray-400',
+        icon: 'text-gray-400 after:bg-no-repeat after:bg-center after:bg-right-line-gray-400-icon'
       },
       size: {
         default: 'h-11 typography-subTitle4 ',
@@ -46,7 +69,7 @@ const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
     if (disabled) {
       return (
         <span
-          className={cn(buttonLinkVariants({ variant, size, className }))}
+          className={cn(buttonLinkDisabledVariants({ variant, size, className }))}
           ref={ref as React.Ref<HTMLSpanElement>}
           {...props}
         >
