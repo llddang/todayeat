6bfcd2cd1db3a2f2a0669a9fbb60e7451cbb128d
hoki9797 <hoki9797@gmail.com>
2025-04-12 20:05:33 +0900

    feat:#69 환경변수 유효성 검사

--- src/lib/utils/gemini-instance.util.ts
+++ src/lib/utils/gemini-instance.util.ts
@@ -1,6 +1,8 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { GoogleGenAI } from '@google/genai';
-import { ENV } from '@/constants/env.constant';
+import { ENV, ENV_ERROR } from '@/constants/env.constant';
+
+if (!ENV.GEMINI_KEY) throw new Error(ENV_ERROR.GEMINI_KEY);
 
 export const generativeAI = new GoogleGenerativeAI(ENV.GEMINI_KEY);
 
