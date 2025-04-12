b13b8a3e193de39e1d0301c54054e8068be4e622
hoki9797 <hoki9797@gmail.com>
2025-04-12 19:44:18 +0900

    chore:#69 함수명 변경 및 반환값 명시

--- src/lib/apis/analysis-request.api.ts
+++ src/lib/apis/analysis-request.api.ts
@@ -1,9 +1,11 @@
 import { camelToSnakeObject } from '@/lib/utils/camelize.util';
 import { getServerClient } from '@/lib/utils/supabase/server.util';
-import { FoodAnalysisResult } from '@/types/gemini.type';
+import { FoodAnalysisRequestsDetailDTO } from '@/types/DTO/food_analysis.dto';
+import { PostgrestError } from '@supabase/supabase-js';
 
 export const getFoodImagesById = async (userId: string) => {
   const supabase = getServerClient();
+
   const { data, error } = await supabase
     .from('food_analysis_requests')
     .select('id, image_urls')
@@ -13,12 +15,16 @@ export const getFoodImagesById = async (userId: string) => {
   return { data, error };
 };
 
-export const saveFoodAnalysisResult = async (requestId: string, result: FoodAnalysisResult[]) => {
+export const createFoodAnalysisResult = async (
+  insertPayload: FoodAnalysisRequestsDetailDTO[]
+): Promise<{
+  error: PostgrestError | null;
+}> => {
   const supabase = getServerClient();
-  const insertPayload = result.map((item) => ({
-    ...item,
-    requestId
-  }));
 
-  return await supabase.from('food_analysis_requests_detail').insert(camelToSnakeObject(insertPayload));
+  const { error } = await supabase
+    .from('food_analysis_requests_detail')
+    .insert(camelToSnakeObject<FoodAnalysisRequestsDetailDTO[]>(insertPayload));
+
+  return { error };
 };
