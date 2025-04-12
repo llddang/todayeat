ac73f3047e1f1918e692c071bc71c0a503f51025
woohyuckk <woohyuckk@gmail.com>
2025-04-12 13:27:20 +0900

    feat:meal-post-memo-textarea 컴포넌트 구현

---
+++ src/components/_meal/_post/meal-post-memo-textarea.tsx
@@ -0,0 +1,41 @@
+import { Textarea } from '@/components/ui/textarea';
+import { Typography } from '@/components/ui/typography';
+import { cn } from '@/lib/utils';
+import * as React from 'react';
+
+type Props = React.ComponentProps<'textarea'> & {
+  charCount?: number;
+};
+
+const MAX_MEMO_LENGTH = 200;
+
+const MealPostMemoTextarea = React.forwardRef<HTMLTextAreaElement, Props>(
+  ({ className, value, charCount = 0, ...props }, ref) => {
+    return (
+      <div
+        className={cn(
+          'group flex h-[12.5rem] w-[20.5625rem] flex-col gap-2 rounded-lg border border-gray-300 bg-white p-4 focus-within:!border-gray-800 hover:border-gray-500',
+          className
+        )}
+      >
+        <textarea
+          className="caret-purple-300 flex-1 resize-none typography-body1 focus:border-none focus:outline-none focus:ring-0 focus-visible:ring-0"
+          value={value}
+          maxLength={MAX_MEMO_LENGTH}
+          ref={ref}
+          {...props}
+        />
+
+        <Typography as="div" variant="body4" className="flex items-center justify-end gap-[0.125rem] text-gray-500">
+          <span className="group-focus-within:text-gray-600 group-hover:text-gray-600">{charCount}</span>
+          <span>/</span>
+          <span>{MAX_MEMO_LENGTH}</span>
+        </Typography>
+      </div>
+    );
+  }
+);
+
+MealPostMemoTextarea.displayName = 'MealPostMemoTextarea';
+
+export default MealPostMemoTextarea;
