ce0c60b6d6bf45f835dca3fed98ea5c625f5fa8e
hoki9797 <hoki9797@gmail.com>
2025-04-12 20:35:35 +0900

    refactor:#69 반환 타입 명시

--- src/lib/apis/analysis-request.api.ts
+++ src/lib/apis/analysis-request.api.ts
@@ -1,9 +1,11 @@
 import { camelToSnakeObject } from '@/lib/utils/camelize.util';
 import { getServerClient } from '@/lib/utils/supabase/server.util';
-import { FoodAnalysisRequestsDetailDTO } from '@/types/DTO/food_analysis.dto';
+import { FoodAnalysisRequestsDetailDTO, FoodAnalysisRequestsDTO } from '@/types/DTO/food_analysis.dto';
 import { PostgrestError } from '@supabase/supabase-js';
 
-export const getFoodImagesById = async (userId: string) => {
+export const getFoodImagesById = async (
+  userId: string
+): Promise<{ data: FoodAnalysisRequestsDTO | null; error: PostgrestError | null }> => {
   const supabase = getServerClient();
 
   const { data, error } = await supabase
