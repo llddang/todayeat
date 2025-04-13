938a6a05062de575eec3309a34e63899328215c3
hoki9797 <hoki9797@gmail.com>
2025-04-13 13:27:01 +0900

    feat:#69 라우트 핸들러 변경

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -1,15 +1,45 @@
 import { NextResponse } from 'next/server';
 import { AI_ERROR_KEYS, AI_ERROR_MESSAGE, isAIErrorResponse } from '@/constants/ai-error-message.constant';
-import { createFoodAnalysisResult, getFoodImagesById } from '@/lib/apis/analysis-request.api';
+import { createFoodAnalysisResult, createFoodImageUrls, getFoodImagesById } from '@/lib/apis/analysis-request.api';
 import { generateFoodAnalysisByImage } from '@/lib/apis/gemini.api';
 import { convertImageUrlToBase64 } from '@/lib/utils/convert-image-to-base64.util';
 import { parseGeminiResponse } from '@/lib/utils/gemini.util';
 import { FoodAnalysisRequestsDetailDTO } from '@/types/DTO/food_analysis.dto';
 import { FoodAnalysisResult, ImageContent } from '@/types/gemini.type';
+import { uploadImage } from '@/lib/apis/storage.api';
 
 export async function POST(req: Request) {
   try {
-    const { userId } = await req.json();
+    const formData = await req.formData();
+    const userId = formData.get('userId')?.toString() as string;
+    const files = formData.getAll('files') as File[];
+
+    const uploadedUrls: string[] = [];
+
+    for (const file of files) {
+      const uploadForm = new FormData();
+      uploadForm.set('file', file);
+
+      const { data: publicUrl, error: uploadError } = await uploadImage('meal-requests', uploadForm);
+
+      if (uploadError || !publicUrl) {
+        console.error('이미지 업로드 실패:', uploadError);
+        return NextResponse.json(isAIErrorResponse(AI_ERROR_KEYS.UNKNOWN), {
+          status: AI_ERROR_MESSAGE.UNKNOWN.status
+        });
+      }
+
+      uploadedUrls.push(publicUrl);
+    }
+
+    const { error: insertTempError } = await createFoodImageUrls(userId, uploadedUrls);
+
+    if (insertTempError) {
+      console.error('임시 테이블 저장 실패:', insertTempError);
+      return NextResponse.json(isAIErrorResponse(AI_ERROR_KEYS.SUPABASE_INSERT_FAILED), {
+        status: AI_ERROR_MESSAGE.SUPABASE_INSERT_FAILED.status
+      });
+    }
 
     const { data, error } = await getFoodImagesById(userId);
 
@@ -23,9 +53,9 @@ export async function POST(req: Request) {
     const requestId: string = data.id;
     const imageUrls: string[] = data.image_urls;
 
-    const imageParts: ImageContent[] = await Promise.all(imageUrls.map(convertImageUrlToBase64));
+    const base64Images: ImageContent[] = await Promise.all(imageUrls.map(convertImageUrlToBase64));
 
-    const generatedTextResult = await generateFoodAnalysisByImage(imageParts);
+    const generatedTextResult = await generateFoodAnalysisByImage(base64Images);
 
     const parsedResult = parseGeminiResponse(generatedTextResult);
 
