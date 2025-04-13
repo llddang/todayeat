c43fed0fc8eaeefcc566011194179767969fe732
llddang <bodyness@naver.com>
2025-04-13 19:53:27 +0900

    design:#99 활성상태/비활성상태 디자인 구성

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ export const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
+      'inline-flex h-8 items-center justify-center rounded-[1.25rem] bg-transparent px-3 text-gray-600 typography-body3 data-[state=active]:bg-white data-[state=active]:text-gray-900 data-[state=active]:typography-subTitle4',
       className
     )}
     {...props}
