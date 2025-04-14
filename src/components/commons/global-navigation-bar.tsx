c7a577d822a64c201fcc9693e69baf256f90b937
hoki9797 <hoki9797@gmail.com>
2025-04-15 08:47:07 +0900

    feat:#104 접근성을 고려한 시멘틱 태그 추가

--- src/components/commons/global-navigation-bar.tsx
+++ src/components/commons/global-navigation-bar.tsx
@@ -18,25 +18,33 @@ const GlobalNavigationBar = () => {
       : HOME;
 
   return (
-    <nav className="flex gap-2 rounded-[6.25rem] bg-gray-200/[0.64] p-2">
-      <GlobalNavigationBarItem
-        href={SITE_MAP.HOME}
-        label="홈"
-        icon={currentPage === HOME ? 'before:bg-home-fill-icon' : 'before:bg-home-line-icon'}
-        active={currentPage === HOME}
-      />
-      <GlobalNavigationBarItem
-        href={SITE_MAP.MEAL_POST}
-        label="식사 기록"
-        icon={currentPage === POST ? 'before:bg-analyze-fill-icon' : 'before:bg-analyze-line-icon'}
-        active={currentPage === POST}
-      />
-      <GlobalNavigationBarItem
-        href={SITE_MAP.REPORT}
-        label="리포트"
-        icon={currentPage === REPORT ? 'before:bg-chart-fill-icon' : 'before:bg-chart-line-icon'}
-        active={currentPage === REPORT}
-      />
+    <nav className="rounded-[6.25rem] bg-gray-200/[0.64] p-2">
+      <ul className="flex gap-2">
+        <li>
+          <GlobalNavigationBarItem
+            href={SITE_MAP.HOME}
+            label="홈"
+            icon={currentPage === HOME ? 'before:bg-home-fill-icon' : 'before:bg-home-line-icon'}
+            active={currentPage === HOME}
+          />
+        </li>
+        <li>
+          <GlobalNavigationBarItem
+            href={SITE_MAP.MEAL_POST}
+            label="식사 기록"
+            icon={currentPage === POST ? 'before:bg-analyze-fill-icon' : 'before:bg-analyze-line-icon'}
+            active={currentPage === POST}
+          />
+        </li>
+        <li>
+          <GlobalNavigationBarItem
+            href={SITE_MAP.REPORT}
+            label="리포트"
+            icon={currentPage === REPORT ? 'before:bg-chart-fill-icon' : 'before:bg-chart-line-icon'}
+            active={currentPage === REPORT}
+          />
+        </li>
+      </ul>
     </nav>
   );
 };
