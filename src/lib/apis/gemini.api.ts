753e433be693bac14e3655637101889d8b2817df
hoki9797 <hoki9797@gmail.com>
2025-04-14 17:26:24 +0900

    feat:#69 칼로리 분석 api 구현

--- src/lib/apis/gemini.api.ts
+++ src/lib/apis/gemini.api.ts
@@ -1,5 +1,5 @@
-import { FOOD_IMAGE_ANALYSIS_PROMPT } from '@/constants/prompt.constant';
-import { getGenerativeAI } from '@/lib/utils/gemini.util';
+import { CALORIES_ANALYSIS_PROMPT, FOOD_IMAGE_ANALYSIS_PROMPT } from '@/constants/prompt.constant';
+import { getGenAI, getGenerativeAI } from '@/lib/utils/gemini.util';
 import { ImageContent } from '@/types/gemini.type';
 
 export const generateFoodAnalysisByImage = async (imageParts: ImageContent[]): Promise<string> => {
@@ -10,3 +10,18 @@ export const generateFoodAnalysisByImage = async (imageParts: ImageContent[]): P
   const result = await model.generateContent([...imageParts, FOOD_IMAGE_ANALYSIS_PROMPT]);
   return result.response.text();
 };
+
+export const generateCaloriesAnalysisByText = async (foodName: string, weight: number): Promise<string> => {
+  const preInfomation = `  
+    음식 이름: ${foodName}
+    음식 중량: ${weight}g
+  `;
+  const response = await getGenAI().models.generateContent({
+    model: 'gemini-2.0-flash',
+    contents: preInfomation + CALORIES_ANALYSIS_PROMPT
+  });
+
+  return response.text ?? NOT_VALID;
+};
+
+const NOT_VALID = '정보가 올바르지 않습니다';
