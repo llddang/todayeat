511ede3b71b59f4b4e5813d2c76006519ea1fd85
woohyuckk <woohyuckk@gmail.com>
2025-04-16 01:05:58 +0900

    feat:#112 meal-post-modal 구현

---
+++ src/components/_meal/_post/meal-post-modal.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+
+type MealPostModalProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onApproveClickHandler: () => void;
+  onCancelClickHandler: () => void;
+};
+
+const MealPostModal = ({ open, onOpenChange, onApproveClickHandler, onCancelClickHandler }: MealPostModalProps) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>이전 정보 발견</AlertDialogTitle>
+
+          <AlertDialogDescription>등록하던 정보가 있습니다. 사용하시겠습니까?</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <Button variant="secondary" onClick={onCancelClickHandler}>
+            아니요
+          </Button>
+          <Button variant="secondary" onClick={onApproveClickHandler}>
+            예
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default MealPostModal;
