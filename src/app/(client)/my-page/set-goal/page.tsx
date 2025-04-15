9fd8971771fef6816dde9abec97e19e00f66281d
jinew <jiyunk@naver.com>
2025-04-15 22:09:49 +0900

    chore:#105 미사용 import 삭제

--- src/app/(client)/my-page/set-goal/page.tsx
+++ src/app/(client)/my-page/set-goal/page.tsx
@@ -1,20 +1,7 @@
 'use client';
 
-import { Suspense } from 'react';
-import useFunnel from '@/lib/hooks/use-funnel';
 import GlassBackground from '@/components/commons/glass-background';
-import SetGoalPurposeStep from '@/components/_set-goal/set-goal-purpose-step';
-import SetGoalGenderStep from '@/components/_set-goal/set-goal-gender-step';
-import SetGoalAgeStep from '@/components/_set-goal/set-goal-age-step';
-import SetGoalHeightStep from '@/components/_set-goal/set-goal-height-step';
-import SetGoalWeightStep from '@/components/_set-goal/set-goal-weight-step';
-import SetGoalActivityLevelStep from '@/components/_set-goal/set-goal-activity-level-step';
-import SetGoalAiLoadingStep from '@/components/_set-goal/set-goal-ai-loading-step';
-import SetGoalCalculateStep from '@/components/_set-goal/set-goal-caculate-step';
-import SetGoalComplete from '@/components/_set-goal/set-goal-complete';
-import { Typography } from '@/components/ui/typography';
 import { useSearchParams } from 'next/navigation';
-import { ActivityLevelType, GenderType, PurposeType } from '@/types/user-personal-info.type';
 import { getPercentage } from '@/lib/utils/nutrition-calculator.util';
 import { FunnelStep } from '@/types/set-goal.type';
 import { LAST_STEP_FOR_USER_INPUT, STEP_UI_CONFIG } from '@/constants/set-goal.constant';
