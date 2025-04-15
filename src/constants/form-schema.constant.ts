c23d14628666ddcdcdedd8d31f2100922bccfbba
jinew <jiyunk@naver.com>
2025-04-15 20:13:45 +0900

    refactor:#105 폼 스키마 상수 추가

--- src/constants/form-schema.constant.ts
+++ src/constants/form-schema.constant.ts
@@ -21,7 +21,19 @@ const FormSchema = {
     .max(16, { message: '비밀번호는 16자리 이하 입력해주세요.' })
     .regex(passwordRegex, { message: '영문, 숫자, 특수문자(~!@#$%^&*)를 최소 1개 이상 포함하여야 합니다.' }),
   CONFIRM_PASSWORD_SCHEMA: z.string().nonempty('필수 입력 항목입니다.'),
-  IMAGE_FILE_SCHEMA: z.custom<File>((data) => data instanceof File, { message: '유효한 File 객체가 아닙니다.' })
+  IMAGE_FILE_SCHEMA: z.custom<File>((data) => data instanceof File, { message: '유효한 File 객체가 아닙니다.' }),
+  ONLY_NUMBER_SCHEMA: z
+    .string()
+    .nonempty('필수 입력 항목입니다')
+    .refine((value) => /^[0-9]+$/.test(value), {
+      message: '0 이상의 숫자만 입력해 주세요'
+    }),
+  NUMBER_WITH_ONE_DECIMAL_SCHEMA: z
+    .string()
+    .nonempty('필수 입력 항목입니다')
+    .refine((value) => /^(0|[1-9]\d*)(\.\d{1})?$/.test(value), {
+      message: '0 이상의 숫자 또는 소수점 첫째 자리까지만 입력해 주세요'
+    })
 };
 
 export default FormSchema;
