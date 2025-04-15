60f2530a727cbfa8210db76238bfbcc5d10a56b9
llddang <bodyness@naver.com>
2025-04-16 03:50:35 +0900

    design:#121 구글 로그인 버튼 디자인 적용

--- src/components/_auth/auth-sign-in-with-google-button.tsx
+++ src/components/_auth/auth-sign-in-with-google-button.tsx
@@ -4,6 +4,14 @@ import { Button } from '@/components/ui/button';
 import { signInWithGoogle } from '@/lib/apis/auth-browser.api';
 
 const AuthSignInWithGoogleButton = () => {
-  return <Button onClick={signInWithGoogle}>구글 로그인</Button>;
+  return (
+    <Button
+      onClick={signInWithGoogle}
+      variant="secondary"
+      className="before:bg-google-logo flex gap-1 before:block before:h-5 before:w-5 before:bg-contain before:bg-center before:bg-no-repeat"
+    >
+      구글로 계속하기
+    </Button>
+  );
 };
 export default AuthSignInWithGoogleButton;
