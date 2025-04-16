996f956109cfc0f96a1b11ca13a312421ab164f2
woohyuckk <woohyuckk@gmail.com>
2025-04-16 10:33:57 +0900

    chore:#112 불필요한 import 삭제

--- src/app/(client)/meal/post/page.tsx
+++ src/app/(client)/meal/post/page.tsx
@@ -1,5 +1,5 @@
 import MealPostContents from '@/components/_meal/_post/meal-post-contents';
-import { getFoodAnalysisDetail, getFoodImagesById } from '@/lib/apis/analysis-request.api';
+import { getFoodAnalysisDetail } from '@/lib/apis/analysis-request.api';
 
 const MealPostPage = async (): Promise<JSX.Element> => {
   const { data } = await getFoodAnalysisDetail();
