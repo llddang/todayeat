79c7a16b5a1f9accea58f9af2d84b9b0b09f8b35
llddang <bodyness@naver.com>
2025-04-16 21:40:33 +0900

    feat:#138 마이 페이지 최대 width 잡기

--- src/app/(client)/my-page/(index)/layout.tsx
+++ src/app/(client)/my-page/(index)/layout.tsx
@@ -8,13 +8,13 @@ const Layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <>
+    <div className="temp-layout">
       <Header />
       <div className="pt-layout">
         <Suspense>{children}</Suspense>
         <Footer />
       </div>
-    </>
+    </div>
   );
 };
 
