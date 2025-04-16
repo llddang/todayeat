37f479f28cbcd072b8603e754c9483543b5a33b6
llddang <bodyness@naver.com>
2025-04-16 17:09:58 +0900

    add:#129 홈 카드 이미지 프리뷰 컴포넌트 구현

---
+++ src/components/_home/home-meal-card-image-preview.tsx
@@ -0,0 +1,14 @@
+import { MealDTO } from '@/types/DTO/meal.dto';
+
+const HomeMealCardImagePreview = ({ images }: { images: MealDTO['foodImages'] }) => {
+  if (!images || images.length === 0) return null;
+  return (
+    <div className="flex [&>*:not(:last-child)]:-mr-3">
+      {images?.map((image) => (
+        <img key={image} className="h-10 w-10 rounded-[0.5rem] border border-white" src={image} />
+      ))}
+    </div>
+  );
+};
+
+export default HomeMealCardImagePreview;
