33ec1cbd6d18dae4528f9f2524655391e9b4ce8a
llddang <bodyness@naver.com>
2025-04-16 17:14:36 +0900

    add:#129 홈 식단 기록이 없을 때 뜨는 빈 카드 구현

---
+++ src/components/_home/home-meal-empty-card.tsx
@@ -0,0 +1,28 @@
+import { Typography } from '@/components/ui/typography';
+import SITE_MAP from '@/constants/site-map.constant';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+const HomeMealEmptyCard = () => {
+  return (
+    <Link
+      href={SITE_MAP.MEAL_POST}
+      className={cn(
+        'flex flex-col items-center justify-center gap-4 rounded-2xl bg-white px-4 py-7 text-center',
+        'before:h-10 before:w-10 before:bg-image-upload-illustration before:bg-contain before:bg-center before:bg-no-repeat'
+      )}
+    >
+      <div>
+        <Typography variant="subTitle2" className="text-gray-900">
+          오늘의 식사를 간편하게 남겨보세요!
+        </Typography>
+        <Typography variant="body3" className="mt-2 text-gray-600">
+          사진을 올리면 AI가 알아서 분석해 드려요.
+          <br />
+          지금 첫 식사를 기록해 보세요!
+        </Typography>
+      </div>
+    </Link>
+  );
+};
+export default HomeMealEmptyCard;
