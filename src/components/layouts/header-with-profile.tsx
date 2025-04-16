1a8c743fcae083e361715aa68630fc54ec2b3e96
hoki9797 <hoki9797@gmail.com>
2025-04-16 15:34:54 +0900

    fix:#109 프로필이 없는 유저의 경우 마이페이지로 이동하도록 수정

--- src/components/layouts/header-with-profile.tsx
+++ src/components/layouts/header-with-profile.tsx
@@ -16,12 +16,12 @@ const HeaderWithProfile = () => {
   return (
     <header
       className={cn(
-        'z-layout fixed left-0 top-0 flex h-16 w-full items-center justify-between px-4 py-3',
+        'fixed left-0 top-0 z-layout flex h-16 w-full items-center justify-between px-4 py-3',
         isScrolled ? 'bg-purple-10/94 backdrop-blur-[20px]' : 'bg-transparent'
       )}
     >
       <Image src={LOGO} alt="투데잇 로고" />
-      {user ? (
+      {user.id ? (
         <Link href={SITE_MAP.MY_PAGE}>
           <ProfileImage src={user.profileImage} size="sm" />
         </Link>
