c42c895ebbec4c77719b1094c81442a3fc63e892
jinew <jiyunk@naver.com>
2025-04-16 14:30:36 +0900

    add:#118 마이페이지 관련 컴포넌트 추가

---
+++ src/components/_my-page/my-page-macronutrient-box.tsx
@@ -0,0 +1,50 @@
+import { Typography } from '@/components/ui/typography';
+import { formatNumberWithComma } from '@/lib/utils/format-number-with-comma';
+import { calculateMacroCaloriesbyGrams, CALORIES_PER_GRAM } from '@/lib/utils/nutrition-calculator.util';
+import React from 'react';
+
+type MyPageMacroNutrientBoxProps = {
+  dailyCarbohydrateGoal: string | number;
+  dailyProteinGoal: string | number;
+  dailyFatGoal: string | number;
+};
+const MyPageMacroNutrientBox = ({
+  dailyCarbohydrateGoal,
+  dailyProteinGoal,
+  dailyFatGoal
+}: MyPageMacroNutrientBoxProps) => {
+  const carbInfo = calculateMacroCaloriesbyGrams(dailyCarbohydrateGoal, CALORIES_PER_GRAM.CARBOHYDRATE);
+  const proteinInfo = calculateMacroCaloriesbyGrams(dailyProteinGoal, CALORIES_PER_GRAM.PROTEIN);
+  const fatInfo = calculateMacroCaloriesbyGrams(dailyFatGoal, CALORIES_PER_GRAM.FAT);
+
+  return (
+    <ul className="mt-3 flex w-full flex-col gap-2 rounded-xl bg-gray-100 p-4">
+      <li className="flex justify-between">
+        <Typography as="span" variant="body1" className="text-gray-600">
+          탄수화물
+        </Typography>
+        <Typography as="span" variant="body1" className="text-gray-800">
+          {`${formatNumberWithComma(carbInfo.grams)}g (${formatNumberWithComma(carbInfo.calories)}kcal)`}
+        </Typography>
+      </li>
+      <li className="flex justify-between">
+        <Typography as="span" variant="body1" className="text-gray-600">
+          단백질
+        </Typography>
+        <Typography as="span" variant="body1" className="text-gray-800">
+          {`${formatNumberWithComma(proteinInfo.grams)}g (${formatNumberWithComma(proteinInfo.calories)}kcal)`}
+        </Typography>
+      </li>
+      <li className="flex justify-between">
+        <Typography as="span" variant="body1" className="text-gray-600">
+          지방
+        </Typography>
+        <Typography as="span" variant="body1" className="text-gray-800">
+          {`${formatNumberWithComma(fatInfo.grams)}g (${formatNumberWithComma(fatInfo.calories)}kcal)`}
+        </Typography>
+      </li>
+    </ul>
+  );
+};
+
+export default MyPageMacroNutrientBox;
---
+++ src/components/_my-page/my-page-user-physical-info-card.tsx
@@ -0,0 +1,43 @@
+import { UserDTO } from '@/types/DTO/user.dto';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Typography } from '@/components/ui/typography';
+import MyPageUserInfoList from '@/components/_my-page/my-page-user-info-list';
+import { ACTIVITY_LEVEL_OPTIONS_IN_PROFILE, GENDER_OPTIONS_IN_PROFILE } from '@/constants/user-personal-info.constant';
+
+type MyPageUserPhysicalInfoCardProps = {
+  userInfo: UserDTO;
+};
+const MyPageUserPhysicalInfoCard = ({ userInfo }: MyPageUserPhysicalInfoCardProps) => {
+  const {
+    gender = 'NOT_SET',
+    height = 'NOT_SET',
+    weight = 'NOT_SET',
+    activityLevel = 'NOT_SET'
+  } = userInfo.personalInfo ?? {};
+
+  return (
+    <Accordion type="single" collapsible className="rounded-2xl bg-white px-4 py-4">
+      <AccordionItem value="item-1 border-none">
+        <AccordionTrigger className="px-1 py-[0.56rem]">
+          <Typography as="h3" variant="subTitle2">
+            신체 정보
+          </Typography>
+        </AccordionTrigger>
+        <AccordionContent className="mt-2 flex flex-col gap-3 border-t-[1px] border-gray-200 pb-1 pt-5">
+          <MyPageUserInfoList
+            title="성별"
+            description={gender === 'NOT_SET' ? '미설정' : GENDER_OPTIONS_IN_PROFILE[gender]}
+          />
+          <MyPageUserInfoList title="키" description={height === 'NOT_SET' ? '미설정' : `${height}cm`} />
+          <MyPageUserInfoList title="몸무게" description={weight === 'NOT_SET' ? '미설정' : `${weight}kg`} />
+          <MyPageUserInfoList
+            title="활동 수준"
+            description={ACTIVITY_LEVEL_OPTIONS_IN_PROFILE[activityLevel].description}
+          />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default MyPageUserPhysicalInfoCard;
