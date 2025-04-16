a51808080bad467940af8707aa7d0f6a4c13abea
llddang <bodyness@naver.com>
2025-04-16 17:15:14 +0900

    feat:#129 홈 페이지에 식단 기록 물 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,12 @@
 import HomeCalendar from '@/components/_home/home-calendar';
+import HomeMealContainer from '@/components/_home/home-meal-container';
 import Footer from '@/components/commons/footer';
+import GlassBackground from '@/components/commons/glass-background';
 import CalendarProvider from '@/lib/contexts/calendar.context';
 import DashboardProvider from '@/lib/contexts/dashboard.context';
 
-const HomePage = () => {
+// TODO: selectedDate를 context가 아닌 queryParam으로 관리해보기.
+const HomePage = async () => {
   return (
     <div>
       <DashboardProvider>
@@ -12,6 +15,9 @@ const HomePage = () => {
         </CalendarProvider>
 
         {/* TODO: recode summary container 들어가기 */}
+        <GlassBackground className="flex flex-col gap-4 pb-8 pt-6">
+          <HomeMealContainer />
+        </GlassBackground>
       </DashboardProvider>
       <Footer />
     </div>
