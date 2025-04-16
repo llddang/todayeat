fbee24c0137c47d5af2f21bfd87ed961a772ecb8
hoki9797 <hoki9797@gmail.com>
2025-04-17 00:30:39 +0900

    feat:#125 서버 api 함수 사용을 위한 라우트 핸들러

---
+++ src/app/api/meal-post/route.ts
@@ -0,0 +1,13 @@
+import { createFoodAnalysisRequestDetail } from '@/lib/apis/analysis-request.api';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  try {
+    const result = await createFoodAnalysisRequestDetail(body);
+    return NextResponse.json(result);
+  } catch (error) {
+    return NextResponse.json({ message: 'insert 실패', error }, { status: 500 });
+  }
+}
