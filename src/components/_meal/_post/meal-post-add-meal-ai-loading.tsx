7e336185d8d83647d2e4fda69d53044015de771a
hoki9797 <hoki9797@gmail.com>
2025-04-16 23:23:18 +0900

    feat:#125 로딩 ui

---
+++ src/components/_meal/_post/meal-post-add-meal-ai-loading.tsx
@@ -0,0 +1,27 @@
+import dynamic from 'next/dynamic';
+import { Typography } from '@/components/ui/typography';
+import { useEffect } from 'react';
+
+const SetGoalAiLoaderLottie = dynamic(() => import('@/components/_set-goal/set-goal-ai-loader-lottie'), {
+  ssr: false
+});
+
+const MealPostAddMealAiLoading = () => {
+  useEffect(() => {
+    setTimeout(() => {}, 2000);
+  }, []);
+  return (
+    <div className="flex flex-col items-center">
+      <SetGoalAiLoaderLottie />
+      <Typography as="span" variant="subTitle1">
+        AI가 음식 정보를 분석 중이에요!
+      </Typography>
+      <Typography as="p" variant="body2" className="pt-2 text-center text-gray-700">
+        조금만 기다려 주세요. <br />
+        입력하신 정보를 바탕으로 분석 결과를 만들고 있어요!
+      </Typography>
+    </div>
+  );
+};
+
+export default MealPostAddMealAiLoading;
