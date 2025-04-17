91414ba8a28735e9330dda3bde6e0a451a0207b4
llddang <bodyness@naver.com>
2025-04-17 11:14:35 +0900

    add:#157 user Initializer 구현

---
+++ src/components/commons/user-initializer.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { initializeAuthListener } from '@/store/user-store';
+import { useEffect } from 'react';
+
+const UserInitialize = () => {
+  useEffect(() => {
+    initializeAuthListener();
+  }, []);
+  return null;
+};
+export default UserInitialize;
