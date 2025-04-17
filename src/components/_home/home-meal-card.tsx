8a2b7b77e494ac0af8c08c5dfd8f7c8d08514f85
llddang <bodyness@naver.com>
2025-04-17 21:11:38 +0900

    refactor:#162 meal card에서 time line 합치기

--- src/components/_home/home-meal-card.tsx
+++ src/components/_home/home-meal-card.tsx
@@ -5,6 +5,9 @@ import HomeMealCardMemo from '@/components/_home/home-meal-card-memo';
 import { MEAL_CATEGORY_OPTIONS } from '@/constants/meal.constant';
 import { calculateTotalNutrition } from '@/lib/utils/nutrition-calculator.util';
 import { MealDTO } from '@/types/DTO/meal.dto';
+import { Typography } from '@/components/ui/typography';
+import { formatAmPmKorean, formatTime } from '@/lib/utils/date.util';
+import { cn } from '@/lib/utils';
 
 type HomeMealCardProps = {
   meal: MealDTO;
@@ -16,7 +19,20 @@ const HomeMealCard = ({ meal }: HomeMealCardProps) => {
 
   return (
     <li className="flex gap-3">
-      <HomeMealCardTimeIndicator mealIcon={mealIcon} ateAt={meal.ateAt} />
+      <div
+        className={cn(
+          'flex flex-col items-center pt-1 text-center',
+          'before:mb-1 before:h-[1.375rem] before:w-[1.375rem] before:bg-contain before:bg-center before:bg-no-repeat',
+          'after:mt-2 after:w-[1px] after:flex-1 after:bg-gray-350',
+          mealIcon
+        )}
+      >
+        <Typography variant="caption2" className="text-gray-500">
+          {formatAmPmKorean(meal.ateAt)}
+          <br />
+          {formatTime(meal.ateAt)}
+        </Typography>
+      </div>
       {/* TODO : Link 태그로 변경 & 식사 상세 페이지로 이동하게끔!! */}
       <div className="flex w-full flex-1 flex-col gap-4 overflow-hidden rounded-2xl bg-white p-4">
         <HomeMealCardTitle mealLabel={mealLabel} calories={calories} images={meal.foodImages} />
