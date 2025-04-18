fd933ed4ec3cb74765f25fee24a752bc93b5eb56
hoki9797 <hoki9797@gmail.com>
2025-04-18 15:39:23 +0900

    refactor:#166 layer가 감싸도록 변경

--- src/components/layouts/header/header-back-button.tsx
+++ src/components/layouts/header/header-back-button.tsx
@@ -1,27 +1,19 @@
 'use client';
 
-import { cn } from '@/lib/utils';
-import { useDetectIsScrolled } from '@/lib/hooks/use-detect-is-scrolled';
 import { useRouter } from 'next/navigation';
+import HeaderLayer from './header-layer';
 
 const HeaderBackButton = () => {
-  const isScrolled = useDetectIsScrolled();
   const router = useRouter();
 
   return (
-    <header
-      className={cn(
-        'fixed left-0 top-0 z-layout flex h-16 w-full items-center py-3 pl-2 pr-4',
-        isScrolled ? 'bg-purple-10/94 backdrop-blur-[20px]' : 'bg-transparent',
-        'temp-layout left-1/2 -translate-x-1/2'
-      )}
-    >
+    <HeaderLayer>
       <button
         onClick={() => router.back()}
         aria-label="뒤로가기"
         className="h-10 w-10 bg-back-line-icon bg-contain bg-center bg-no-repeat"
       />
-    </header>
+    </HeaderLayer>
   );
 };
 
--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,27 +1,18 @@
 'use client';
 
 import Image from 'next/image';
-import { cn } from '@/lib/utils';
-import { useDetectIsScrolled } from '@/lib/hooks/use-detect-is-scrolled';
 import LOGO from '@/../public/logo.svg';
 import Link from 'next/link';
 import SITE_MAP from '@/constants/site-map.constant';
+import HeaderLayer from './header-layer';
 
 const Header = () => {
-  const isScrolled = useDetectIsScrolled();
-
   return (
-    <header
-      className={cn(
-        'fixed left-0 top-0 z-layout flex h-16 w-full items-center px-4 py-3',
-        isScrolled ? 'bg-purple-10/94 backdrop-blur-[20px]' : 'bg-transparent',
-        'temp-layout left-1/2 -translate-x-1/2'
-      )}
-    >
+    <HeaderLayer>
       <Link href={SITE_MAP.HOME}>
         <Image src={LOGO} alt="투데잇 로고" />
       </Link>
-    </header>
+    </HeaderLayer>
   );
 };
 
