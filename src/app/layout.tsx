4ebb3aa268f8d2d11b3c017390d2ff43436b9e51
jinew <jiyunk@naver.com>
2025-04-20 20:34:42 +0900

    Revert "feat:#178 viewportFit 추가"
    This reverts commit 3202b77585b29a96b021fb45de44c4e197642a01.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,7 @@ const wantedSans = localFont({
 
 export const metadata: Metadata = {
   title: 'Todayeat',
-  description: '사진을 찍기만 하면 음식을 자동으로 인식하고 영양 정보를 기록해주는 웹 애플리케이션.',
-  viewport: {
-    viewportFit: 'cover'
-  }
+  description: '사진을 찍기만 하면 음식을 자동으로 인식하고 영양 정보를 기록해주는 웹 애플리케이션.'
 };
 
 const RootLayout = ({
