cfdf1900bd2cd519e6c26ab04e41bc5b328f9382
woohyuckk <woohyuckk@gmail.com>
2025-04-21 16:35:02 +0900

    feat:#183 메타 태그 설정 및 정보없을시 리다이렉션 옵션 추가

--- src/app/(client)/meal/post/edit/page.tsx
+++ src/app/(client)/meal/post/edit/page.tsx
@@ -1,22 +1,37 @@
+import { redirect } from 'next/navigation';
+import { Metadata } from 'next';
 import { getAiResponses, getFoodImagesById } from '@/apis/analysis-request.api';
 import { getUser } from '@/apis/user.api';
 import EditResultSection from './components/edit-result-section';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const { id: userId } = await getUser();
+  const { data: imageList } = await getFoodImagesById(userId);
+  const aiResponses = await getAiResponses();
+
+  if (!userId || !imageList || aiResponses.length === 0) {
+    return {
+      title: '등록된 식사정보가 없습니다 | 투데잇',
+      description: '식사 정보를 먼저 등록해주세요.'
+    };
+  }
+
+  return {
+    title: '음식 등록 | 투데잇',
+    description: '오늘 먹은 음식을 등록하고 기록해보세요.'
+  };
+};
 
 const MealPostEditPage = async (): Promise<JSX.Element> => {
   const { id: userId } = await getUser();
   const { data: imageList } = await getFoodImagesById(userId);
   const aiResponses = await getAiResponses();
 
-  if (!userId) {
-    return <div>로그인 세션이 만료되었습니다. 로그인을 다시해주세요.</div>;
+  if (!userId || !imageList || aiResponses.length === 0) {
+    redirect('/meal/post');
   }
-  if (!imageList || aiResponses.length === 0) return <div>등록된 음식 정보가 없습니다.</div>;
 
-  return (
-    <div>
-      <EditResultSection imageList={imageList.imageUrls} mealList={aiResponses} />
-    </div>
-  );
+  return <EditResultSection imageList={imageList.imageUrls} mealList={aiResponses} />;
 };
 
 export default MealPostEditPage;
