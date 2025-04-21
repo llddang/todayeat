6742db487b7895f0ad792a1817333dd64946cbbc
hoki9797 <hoki9797@gmail.com>
2025-04-21 14:22:53 +0900

    fix:#180 헤더에 배경이 적용 안 되는 문제 해결

--- src/components/layouts/header/header-wrapper.tsx
+++ src/components/layouts/header/header-wrapper.tsx
@@ -11,7 +11,7 @@ const HeaderLayer = ({ children }: { children: ReactNode | ReactNode[] }) => {
     <header
       className={cn(
         'fixed left-0 top-0 z-layout flex h-16 w-full items-center justify-between px-4 py-3',
-        isScrolled ? 'bg-purple-10/94 backdrop-blur-[20px]' : 'bg-transparent',
+        isScrolled ? 'bg-purple-10/[0.94] backdrop-blur-[20px]' : 'bg-transparent',
         'left-1/2 -translate-x-1/2 temp-layout'
       )}
     >
