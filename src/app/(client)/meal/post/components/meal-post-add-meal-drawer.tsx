9bf3fa0fcc990df03f9feaf98ec81e2f343c96be
woohyuckk <woohyuckk@gmail.com>
2025-04-22 16:19:07 +0900

    feat:#199 Drawer 로직 수정

--- src/app/(client)/meal/post/components/meal-post-add-meal-drawer.tsx
+++ src/app/(client)/meal/post/components/meal-post-add-meal-drawer.tsx
@@ -3,20 +3,29 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Drawer, DrawerTrigger, DrawerContent, DrawerTitle, DrawerFooter, DrawerClose } from '@/components/ui/drawer';
-import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
+import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import IconButton from '@/components/commons/icon-button';
 import { Typography } from '@/components/ui/typography';
 import MealPostAddMealAiLoading from '@/app/(client)/meal/post/components/meal-post-add-meal-ai-loading';
 import { useUserStore } from '@/store/user-store';
+import { generateCaloriesAnalysisByText } from '@/apis/gemini.api';
+import { parseGeminiResponse } from '@/lib/gemini';
+import { AiResponseDTO } from '@/types/DTO/ai_analysis.dto';
+import { formatNumberWithComma } from '@/utils/format.util';
+import { parseNumber } from '../edit/utils/meal-edit.util';
 
 type FoodFormValues = {
   menuName: string;
   weight: string;
 };
 
-const MealPostAddMealDrawer = () => {
+type MealPostAddMealDrawerProps = {
+  onAddMeal: (meal: AiResponseDTO) => void;
+};
+
+const MealPostAddMealDrawer = ({ onAddMeal }: MealPostAddMealDrawerProps) => {
   const user = useUserStore((state) => state.user);
   const [isAnalyzing, setIsAnalyzing] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
@@ -34,42 +43,25 @@ const MealPostAddMealDrawer = () => {
     try {
       setIsAnalyzing(true);
 
-      const requestRes = await fetch('/api/meal-post', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          menuName: data.menuName,
-          weight: data.weight ? Number(data.weight) : 0
-        })
-      });
+      const generatedTextResult = await generateCaloriesAnalysisByText(
+        data.menuName,
+        data.weight ? Number(data.weight) : 0
+      );
+      const parsedResult = parseGeminiResponse(generatedTextResult);
+      const aiResult = parsedResult[0];
 
-      if (!requestRes.ok) {
-        const errorData = await requestRes.json();
-        throw new Error(errorData.message || '요청 생성 실패');
-      }
+      const newMeal = {
+        id: crypto.randomUUID(),
+        userId: user.id,
+        menuName: data.menuName,
+        weight: aiResult.weight,
+        calories: aiResult.calories,
+        carbohydrate: aiResult.carbohydrate,
+        protein: aiResult.protein,
+        fat: aiResult.fat
+      };
 
-      const requestDetail = await requestRes.json();
-
-      const geminiRes = await fetch('/api/gemini/calories-analysis', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          id: requestDetail.id,
-          userId: user.id,
-          menuName: requestDetail.menu_name,
-          weight: requestDetail.weight
-        })
-      });
-
-      if (!geminiRes.ok) {
-        const errorData = await geminiRes.json();
-        throw new Error(errorData.message || 'AI 분석 실패');
-      }
-
+      onAddMeal(newMeal);
       form.reset();
       setIsAnalyzing(false);
       setIsOpen(false);
@@ -111,55 +103,58 @@ const MealPostAddMealDrawer = () => {
         {isAnalyzing ? (
           <MealPostAddMealAiLoading />
         ) : (
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-              <div className="space-y-4">
-                <FormField
-                  control={form.control}
-                  name="menuName"
-                  render={({ field }) => (
-                    <FormItem className="space-y-2">
-                      <FormLabel>먹은 음식</FormLabel>
-                      <FormControl>
-                        <Input placeholder="예시) 김치찌개, 닭가슴살, 크림파스타" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="weight"
-                  render={({ field }) => (
-                    <FormItem className="space-y-2">
-                      <FormLabel>먹은 양 (선택)</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          inputMode="numeric"
-                          measure="g"
-                          placeholder="숫자를 입력할 수 있어요"
-                          onChange={(e) => {
-                            const value = e.target.value;
-                            if (/^\d*$/.test(value)) {
-                              field.onChange(value);
-                            }
-                          }}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+          <div className="space-y-2">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="menuName"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>먹은 음식</FormLabel>
+                    <FormControl>
+                      <Input placeholder="예시) 김치찌개, 닭가슴살, 크림파스타" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="weight"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>먹은 양 (선택)</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        inputMode="numeric"
+                        maxLength={4}
+                        measure="g"
+                        placeholder="숫자를 입력할 수 있어요"
+                        value={field.value ? formatNumberWithComma(Number(field.value)) : ''}
+                        onChange={(e) => {
+                          field.onChange(parseNumber(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-              <DrawerFooter>
-                <Button type="submit" disabled={!canSubmit} className="typography-subTitle4">
-                  AI에게 분석 요청하기
-                </Button>
-              </DrawerFooter>
-            </form>
-          </Form>
+            <DrawerFooter>
+              <Button
+                type="button"
+                disabled={!canSubmit}
+                className="typography-subTitle4"
+                onClick={form.handleSubmit(onSubmit)}
+              >
+                AI에게 분석 요청하기
+              </Button>
+            </DrawerFooter>
+          </div>
         )}
       </DrawerContent>
     </Drawer>
