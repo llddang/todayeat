cffc6f99027f23ae993022a3036f4c9005aba962
hoki9797 <hoki9797@gmail.com>
2025-04-23 10:06:37 +0900

    feat:#198 ChartContainer로 교체

--- src/app/(client)/report/components/weekly.tsx
+++ src/app/(client)/report/components/weekly.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import GlassBackground from '@/components/commons/glass-background';
+import MacroNutrientPieChart from '@/components/commons/macronutrient-pie-chart';
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { Typography } from '@/components/ui/typography';
-import { Bar, BarChart, Cell, ReferenceLine, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Cell, ReferenceLine, XAxis, YAxis } from 'recharts';
 
 const Weekly = () => {
   return (
-    <GlassBackground className="min-h-full w-full rounded-2xl p-4">
-      <Typography as="h2" variant="subTitle1">
-        이번 주는 저번 주보다 <br /> 250kcal 덜 먹었어요
-      </Typography>
-      <div className="h-48">
-        <ResponsiveContainer width="100%" height="100%">
+    <>
+      <GlassBackground className="min-h-full w-full rounded-2xl p-4">
+        <Typography as="h2" variant="subTitle1">
+          이번 주는 저번 주보다 <br /> 250kcal 덜 먹었어요
+        </Typography>
+        <ChartContainer config={chartConfig} className="mt-4 min-h-full w-full">
           <BarChart data={data} barSize={32}>
             <XAxis
               dataKey="label"
@@ -21,6 +23,7 @@ const Weekly = () => {
               tick={{ fill: '#B1B1B1' }}
             />
             <YAxis hide />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel nameKey="calories" />} />
             <Bar dataKey="value" radius={[4, 4, 4, 4]}>
               {data.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={entry.label === '이번 주' ? '#FFE37E' : '#FFF5CC'} />
@@ -35,49 +38,60 @@ const Weekly = () => {
               strokeLinejoin="round"
             />
           </BarChart>
-        </ResponsiveContainer>
-      </div>
+        </ChartContainer>
 
-      <div className="flex flex-col gap-3 rounded-xl bg-white p-4 text-gray-700">
-        <div className="flex items-center justify-between">
-          <Typography as="span" variant="body3">
-            이번 주 평균 섭취 칼로리
-          </Typography>
-          <div className="space-x-1">
-            <Typography as="span" variant="subTitle4" className="text-gray-900">
-              1,780
-            </Typography>
+        <div className="flex flex-col gap-3 rounded-xl bg-white p-4 text-gray-700">
+          <div className="flex items-center justify-between">
             <Typography as="span" variant="body3">
-              kcal
+              이번 주 평균 섭취 칼로리
             </Typography>
+            <div className="space-x-1">
+              <Typography as="span" variant="subTitle4" className="text-gray-900">
+                1,780
+              </Typography>
+              <Typography as="span" variant="body3">
+                kcal
+              </Typography>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <Typography as="span" variant="body3">
+              1일 목표 칼로리
+            </Typography>
+            <div className="space-x-1">
+              <Typography as="span" variant="subTitle4" className="text-gray-900">
+                1,650
+              </Typography>
+              <Typography as="span" variant="body3">
+                kcal
+              </Typography>
+            </div>
           </div>
         </div>
-        <div className="flex items-center justify-between">
-          <Typography as="span" variant="body3">
-            1일 목표 칼로리
-          </Typography>
-          <div className="space-x-1">
-            <Typography as="span" variant="subTitle4" className="text-gray-900">
-              1,650
-            </Typography>
-            <Typography as="span" variant="body3">
-              kcal
-            </Typography>
-          </div>
-        </div>
-      </div>
-    </GlassBackground>
+      </GlassBackground>
+      <GlassBackground className="min-h-full w-full rounded-2xl p-4">
+        <MacroNutrientPieChart data={chartData} />
+      </GlassBackground>
+    </>
   );
 };
 
 export default Weekly;
 
 const data = [
-  { label: '~2.23', value: 1600 },
-  { label: '~3.2', value: 1200 },
-  { label: '~3.9', value: 1300 },
-  { label: '~3.16', value: 1700 },
-  { label: '~3.23', value: 2000 },
-  { label: '~3.30', value: 1800 },
-  { label: '이번 주', value: 1780 }
+  { label: '~2.23', value: 1600, fill: '#FFF5CC' },
+  { label: '~3.2', value: 1200, fill: '#FFF5CC' },
+  { label: '~3.9', value: 1300, fill: '#FFF5CC' },
+  { label: '~3.16', value: 1700, fill: '#FFF5CC' },
+  { label: '~3.23', value: 2000, fill: '#FFF5CC' },
+  { label: '~3.30', value: 1800, fill: '#FFF5CC' },
+  { label: '이번 주', value: 1780, fill: '#FFE37E' }
 ];
+const chartData = { carbohydrate: 500, protein: 200, fat: 300, calories: 1000 };
+
+const chartConfig = {
+  calories: {
+    label: '칼로리',
+    color: '#FFF5CC'
+  }
+} as const satisfies ChartConfig;
