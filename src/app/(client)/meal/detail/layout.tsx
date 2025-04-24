4338ce484f47f0c790ddf27318ee83b188e3e291
woohyuckk <woohyuckk@gmail.com>
2025-04-24 12:27:24 +0900

    feat:#216 임시 레이아웃 추가

---
+++ src/app/(client)/meal/detail/layout.tsx
@@ -0,0 +1,22 @@
+import Header from '@/components/layouts/header';
+import Footer from '@/components/commons/footer';
+import GlobalNavigationBar from '@/components/layouts/global-navigation-bar';
+
+const Layout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="layout-container">
+      <Header variant="withProfile" />
+      <div className="py-layout">
+        <main>{children}</main>
+        <Footer />
+      </div>
+      <GlobalNavigationBar />
+    </div>
+  );
+};
+
+export default Layout;
