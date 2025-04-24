133361a97dcb598e085b971745f8d071b1b8f6af
woohyuckk <woohyuckk@gmail.com>
2025-04-24 20:16:27 +0900

    design:#216 min-width 수정

--- src/app/(client)/meal/detail/components/total-nutrition-chart.tsx
+++ src/app/(client)/meal/detail/components/total-nutrition-chart.tsx
@@ -13,7 +13,7 @@ const TotalNutritionChart = ({ totalNutrition }: TotalNutritionChartProps) => {
       <Typography as="span" variant="body1" className="px-1">
         총 영양 정보
       </Typography>
-      <div className="flex min-h-[13.125rem] w-full min-w-0 items-center justify-center gap-3 rounded-2xl bg-white/50 p-3 backdrop-blur-[50px]">
+      <div className="min-w-auto flex min-h-[13.125rem] w-full items-center justify-center gap-3 rounded-2xl bg-white/50 p-3 backdrop-blur-[50px]">
         <MacroNutrientPieChart data={totalNutrition} displayCalories={true} className="w-[14.5625rem]" />
         <div className="flex w-[5.75rem] flex-col items-start justify-center gap-4 px-1">
           <MacronutrientBox variety={MacronutrientEnum.CARBOHYDRATE} value={totalNutrition.carbohydrate} />
