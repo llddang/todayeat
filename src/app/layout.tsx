31d01212525aac71805e3be8817e109a38432795
llddang <laliddang@gmail.com>
2025-04-24 17:55:37 +0900

    add:#220 project url 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ErrorHandler from '@/components/commons/error-handler';
 import './globals.css';
 import MainBackground from '@/components/layouts/main-background';
 import UserInitialize from '@/components/commons/user-initializer';
+import { ENV } from '@/constants/env.constant';
 
 const wantedSans = localFont({
   src: '../../public/fonts/WantedSansVariable.woff2',
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     locale: 'ko_KR',
-    url: 'https://jiwooda.vercel.app/',
+    url: ENV.PROJECT_URL,
     title: '투데잇 | todayeat',
     description: '먹기 전에 찰칵! 사진 한 장으로 내 식단을 똑똑하게 분석해주는 스마트 식단 관리 서비스',
     siteName: '투데잇',
