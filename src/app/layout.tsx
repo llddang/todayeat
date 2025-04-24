b8ed79f053f802be9078ad4078a24043bcbd16a0
llddang <laliddang@gmail.com>
2025-04-24 15:35:16 +0900

    feat:#209 title 명 Todayeat -> todayeat으로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const wantedSans = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'Todayeat',
+  title: 'todayeat',
   description: '사진을 찍기만 하면 음식을 자동으로 인식하고 영양 정보를 기록해주는 웹 애플리케이션.'
 };
 
