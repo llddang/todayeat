9a55fd84a811b724b3e0fc87b17a1ab1f810abd1
llddang <laliddang@gmail.com>
2025-04-26 23:47:21 +0900

    feat:#209 비밀번호 수정 페이지 전체 맥락 수정 (단일 컴포넌트 -> funnel)

--- src/app/(auth)/find-password/page.tsx
+++ src/app/(auth)/find-password/page.tsx
@@ -1,13 +1,25 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
+import { cn } from '@/lib/shadcn';
 import GlassBackground from '@/components/commons/glass-background';
-import StepVerifyingEmail from './components/step-verifying-email';
+import FindPasswordFunnel from './components/find-password-funnel';
+import { FindPasswordFunnelStep } from './types/funnel-type';
 
-const ResetPasswordPage = () => {
-  return (
-    <GlassBackground className="px-5 pb-6 pt-7 xl:min-h-0 xl:rounded-[2rem] xl:p-10">
-      <StepVerifyingEmail />
-    </GlassBackground>
-  );
+const FindPasswordPage = () => {
+  const params = useSearchParams();
+  const currentStep = (params.get('step') || 'step1') as FindPasswordFunnelStep;
+
+  const isInStep = currentStep !== 'complete';
+
+  if (isInStep) {
+    return (
+      <GlassBackground className={cn('px-5 pb-6 pt-7', 'desktop-width xl:h-fit xl:min-h-0 xl:rounded-[2rem] xl:p-10')}>
+        <FindPasswordFunnel />
+      </GlassBackground>
+    );
+  }
+
+  return <FindPasswordFunnel />;
 };
-export default ResetPasswordPage;
+export default FindPasswordPage;
