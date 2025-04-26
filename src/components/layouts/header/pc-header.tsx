7cb744472e696edb3a8a68ce4bdbd7d051c41340
hoki9797 <hoki9797@gmail.com>
2025-04-27 03:28:42 +0900

    feat:#233 헤더가 항상 배경을 갖도록 변경

--- src/components/layouts/header/pc-header.tsx
+++ src/components/layouts/header/pc-header.tsx
@@ -1,20 +1,9 @@
-'use client';
-
-import { cn } from '@/lib/shadcn';
-import { useDetectIsScrolled } from '@/hooks/use-detect-is-scrolled';
 import WithProfile from './variants/with-profile';
 import GlobalNavigationBar from '../global-navigation-bar';
 
 const PcHeader = () => {
-  const isScrolled = useDetectIsScrolled();
-
   return (
-    <header
-      className={cn(
-        'fixed left-0 right-0 top-0 z-layout h-16 py-3 layout-container',
-        isScrolled ? 'bg-purple-10/[0.94] backdrop-blur-[20px]' : 'bg-transparent'
-      )}
-    >
+    <header className="fixed left-0 right-0 top-0 z-layout h-16 bg-purple-10/[0.94] py-3 backdrop-blur-[20px] layout-container">
       <div className="relative mx-auto flex h-full w-full max-w-[73.75rem] items-center justify-between">
         <WithProfile />
         <GlobalNavigationBar className="absolute bottom-auto top-0 h-fit w-fit p-1 xl:grid" />
