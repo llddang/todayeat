042099a455d613623ee38d2ea9ea3b64f22b1846
jiyxxn <jiyunk@naver.com>
2025-04-26 23:19:39 +0900

    refactor:#229 useState, useEffect 제거, Drawer 컴포넌트화

--- src/app/(client)/my-page/components/profile-bar.tsx
+++ src/app/(client)/my-page/components/profile-bar.tsx
@@ -1,34 +1,19 @@
 'use client';
 
 import { Typography } from '@/components/ui/typography';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger
-} from '@/components/ui/drawer';
 import { useEffect, useState } from 'react';
 import { HorizontalSkeleton } from '@/components/commons/horizontal-skeleton';
 import { useUserStore } from '@/store/user-store';
-import EditProfile from '@/app/(client)/my-page/components/edit-profile';
 import ProfileImage from '@/components/commons/profile-image';
+import EditProfileDrawer from './edit-profile-drawer';
 
 const ProfileBar = () => {
   const user = useUserStore((state) => state.user);
-  const [open, setOpen] = useState(false);
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
-  const [isImageLoading, setIsImageLoading] = useState(true);
+  const isDataLoaded = !!user;
+  const [isProfileImageLoading, setIsProfileImageLoading] = useState(true);
 
   useEffect(() => {
-    if (user.id) setIsDataLoaded(!!user.id);
-  }, [user]);
-
-  useEffect(() => {
-    setIsImageLoading(true);
+    setIsProfileImageLoading(true);
   }, [user.profileImage]);
 
   return (
@@ -40,8 +25,8 @@ const ProfileBar = () => {
               <ProfileImage
                 src={user.profileImage}
                 size="md"
-                isImageLoading={isImageLoading}
-                setIsImageLoading={setIsImageLoading}
+                isImageLoading={isProfileImageLoading}
+                setIsImageLoading={setIsProfileImageLoading}
               />
             </div>
             <div className="flex flex-1 flex-col">
@@ -53,43 +38,8 @@ const ProfileBar = () => {
               </Typography>
             </div>
           </div>
-
-          <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerTrigger asChild>
-              <span
-                className="flex h-9 w-9 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-white/80 bg-center bg-no-repeat object-contain backdrop-blur-[10px] before:block before:h-4 before:w-4 before:bg-edit-info-icon before:bg-contain before:bg-no-repeat before:content-[''] hover:bg-gray-300/80"
-                aria-label="프로필 수정하기"
-                aria-haspopup="dialog"
-                aria-expanded={open}
-              >
-                <span className="sr-only">프로필 수정하기</span>
-              </span>
-            </DrawerTrigger>
-            {open && (
-              <DrawerContent className="rounded-t-[2rem] px-5 pt-4">
-                <div className="relative">
-                  <DrawerHeader className="p-0 text-left">
-                    <DrawerTitle className="py-[0.56rem] pl-1">
-                      <Typography as="span" variant="subTitle2" className="py-0">
-                        프로필 수정
-                      </Typography>
-                    </DrawerTitle>
-                    <DrawerDescription className="sr-only">
-                      여기에서 프로필을 수정하세요. 완료되면 저장 버튼을 클릭하세요.
-                    </DrawerDescription>
-                  </DrawerHeader>
-                  <EditProfile userInfo={user} setOpen={setOpen} />
-                  <DrawerFooter>
-                    <DrawerClose className="absolute right-0 top-0">
-                      <span className="before:block before:h-10 before:w-10 before:bg-close-line-gray-550-icon before:content-['']">
-                        <span className="sr-only">닫기</span>
-                      </span>
-                    </DrawerClose>
-                  </DrawerFooter>
-                </div>
-              </DrawerContent>
-            )}
-          </Drawer>
+          {/* 프로필 수정 Drawer */}
+          <EditProfileDrawer userInfo={user} />
         </>
       ) : (
         <div className="flex min-h-[4.5rem] items-center">
