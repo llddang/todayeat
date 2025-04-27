b35ea68d4eb577bbbd305f1ed2134faeedef6e09
llddang <laliddang@gmail.com>
2025-04-27 20:32:26 +0900

    refactor:#209 data open과 close class 분리

--- src/components/commons/modal.tsx
+++ src/components/commons/modal.tsx
@@ -9,6 +9,7 @@ import {
   AlertDialogPortal,
   AlertDialogTitle
 } from '@radix-ui/react-alert-dialog';
+import { cn } from '@/lib/shadcn';
 
 type InformationModalProps = {
   title: string;
@@ -19,6 +20,7 @@ type InformationModalProps = {
   onClose?: () => void;
   confirmText?: string;
   onConfirm?: () => void;
+  className?: string;
 };
 
 const Modal = ({
@@ -29,7 +31,8 @@ const Modal = ({
   closeText = '닫기',
   onClose,
   confirmText,
-  onConfirm
+  onConfirm,
+  className
 }: InformationModalProps) => {
   return (
     <AlertDialog open={open} onOpenChange={onOpenChange}>
@@ -38,7 +41,14 @@ const Modal = ({
           onClick={() => onClose?.()}
           className="z-modal fixed inset-0 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
         />
-        <AlertDialogContent className="fixed left-[50%] top-[50%] z-50 grid w-fit min-w-[22rem] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-6 rounded-2xl bg-background bg-white p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
+        <AlertDialogContent
+          className={cn(
+            'fixed left-[50%] top-[50%] z-50 grid w-fit min-w-[22rem] max-w-lg -translate-x-1/2 -translate-y-1/2 gap-6 rounded-2xl bg-background bg-white p-6 duration-200',
+            'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]',
+            'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+            className
+          )}
+        >
           <div className="space-y-3 pl-1 text-left">
             <AlertDialogTitle asChild>
               <Typography as="h3" variant="title2" className="text-gray-900">
