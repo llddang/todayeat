5ecb07466228a5c4a9de0bf5930839616ab9f62b
llddang <laliddang@gmail.com>
2025-04-29 00:05:20 +0900

    feat:#252 게스트 로그인 이후 홈으로 리다이렉트 되도록 수정

--- src/app/(auth)/sign-in/components/anonymous-sign-in-button.tsx
+++ src/app/(auth)/sign-in/components/anonymous-sign-in-button.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { SignInAnonymously } from '@/apis/auth-server.api';
 import { Button } from '@/components/ui/button';
+import SITE_MAP from '@/constants/site-map.constant';
 
 const AnonymousSignInButton = () => {
+  const router = useRouter();
+
   return (
     <Button
-      onClick={() => SignInAnonymously()}
+      onClick={() => {
+        SignInAnonymously();
+        router.push(SITE_MAP.HOME);
+      }}
       className="flex gap-2 before:block before:h-[1.125rem] before:w-[1.125rem] before:bg-user-fill-icon before:bg-contain before:bg-center before:bg-no-repeat"
     >
       게스트 로그인
