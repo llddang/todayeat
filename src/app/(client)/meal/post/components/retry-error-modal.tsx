5de06e2d4e43eaaa85ac948c7c69ca7d493f8c94
woohyuckk <woohyuckk@gmail.com>
2025-04-28 12:43:52 +0900

    refactor:#211 공통 Modal로 리팩토링

--- src/app/(client)/meal/post/components/retry-error-modal.tsx
+++ src/app/(client)/meal/post/components/retry-error-modal.tsx
@@ -1,6 +1,5 @@
-import { Button } from '@/components/ui/button';
+import Modal from '@/components/commons/modal';
 import { Typography } from '@/components/ui/typography';
-import { Dialog, DialogContent, DialogOverlay, DialogTitle } from '@radix-ui/react-dialog';
 import { useRouter } from 'next/navigation';
 type RetryErrorModalProps = {
   open: boolean;
@@ -11,41 +10,38 @@ const RetryErrorModal = ({ open, onOpenRetryErrorModalChange }: RetryErrorModalP
   const router = useRouter();
 
   const handleRetryClick = () => {
-    onOpenRetryErrorModalChange(false);
     router.refresh();
   };
 
   return (
-    <Dialog open={open} onOpenChange={onOpenRetryErrorModalChange}>
-      <DialogOverlay className="z-modal fixed inset-0 flex items-center justify-center bg-black/80">
-        <DialogContent className="z-modal absolute left-1/2 top-1/2 flex w-[22.0625rem] -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-6 rounded-2xl bg-white p-6">
+    <Modal
+      open={open}
+      onOpenChange={onOpenRetryErrorModalChange}
+      title="😅 AI가 조금 헷갈려했어요"
+      content={
+        <>
           <div className="flex w-full flex-col items-start gap-3 pl-1">
-            <DialogTitle className="flex w-full items-start gap-2">
-              <Typography as="span" variant="title3">
-                😅 AI가 조금 헷갈려했어요
-              </Typography>
-            </DialogTitle>
             <div className="flex w-full flex-col items-start gap-4 pb-1 pr-1">
               <Typography as="span" variant="body2" className="text-gray-700">
                 이런 사진은 AI가 인식하기 어려워 할 수 있어요
               </Typography>
-              <div className="flex w-full flex-col items-start gap-3 rounded-xl bg-gray-100 p-5">
-                <div className="before:bg-illust-failed-img-1 flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
+              <ul className="flex w-full flex-col items-start gap-3 rounded-xl bg-gray-100 p-5">
+                <li className="flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-illust-failed-img-1 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
                   <Typography as="span" variant="body2" className="text-gray-700">
                     너무 가까이 찍은 사진
                   </Typography>
-                </div>
-                <div className="before:bg-illust-failed-img-2 flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
+                </li>
+                <li className="flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-illust-failed-img-2 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
                   <Typography as="span" variant="body2" className="text-gray-700">
                     음식보다 배경이 중심인 사진
                   </Typography>
-                </div>
-                <div className="before:bg-illust-failed-img-3 flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
+                </li>
+                <li className="flex items-center gap-2 before:block before:h-6 before:w-6 before:bg-illust-failed-img-3 before:bg-contain before:bg-center before:bg-no-repeat before:content-['']">
                   <Typography as="span" variant="body2" className="text-gray-700">
                     빛 반사가 심한 사진
                   </Typography>
-                </div>
-              </div>
+                </li>
+              </ul>
               <Typography as="span" variant="body2" className="text-gray-700">
                 사진을 다시 업로드하거나
                 <br />
@@ -53,12 +49,12 @@ const RetryErrorModal = ({ open, onOpenRetryErrorModalChange }: RetryErrorModalP
               </Typography>
             </div>
           </div>
-          <Button variant="primary" onClick={handleRetryClick} className="w-full">
-            다시 시도하기
-          </Button>
-        </DialogContent>
-      </DialogOverlay>
-    </Dialog>
+        </>
+      }
+      closeText=""
+      confirmText="다시 시도하기"
+      onConfirm={handleRetryClick}
+    />
   );
 };
 
