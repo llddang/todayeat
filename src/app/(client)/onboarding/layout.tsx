5058f052aa2b4b629e5a6099fdbf178af79d4138
jiyxxn <jiyunk@naver.com>
2025-04-28 10:00:26 +0900

    add:#239 온보딩 레이아웃 구현

---
+++ src/app/(client)/onboarding/layout.tsx
@@ -0,0 +1,30 @@
+import { Suspense } from 'react';
+import Header from '@/components/layouts/header';
+import Footer from '@/components/commons/footer';
+import { Metadata } from 'next';
+import GlobalNavigationBar from '@/components/layouts/global-navigation-bar';
+
+export const metadata: Metadata = {
+  title: '온보딩 | 투데잇',
+  description: '수정 필요',
+  robots: 'noindex'
+};
+
+const Layout = ({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="bg-white layout-container">
+      <Header variant="withProfile" />
+      <div className="bg-gradient-onboarding mx-auto pb-36 pt-layout xl:pt-20">
+        <Suspense>{children}</Suspense>
+        <Footer />
+        <GlobalNavigationBar />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
