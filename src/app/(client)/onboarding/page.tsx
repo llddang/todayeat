4fc87d1d7b49972dfd4dab3c203504da3ef8d31c
jiyxxn <jiyunk@naver.com>
2025-04-28 10:00:13 +0900

    add:#239 온보딩 페이지 구현

---
+++ src/app/(client)/onboarding/page.tsx
@@ -0,0 +1,32 @@
+import { cookies } from 'next/headers';
+import FirstSection from './components/first-section';
+import SecondSection from './components/second-section';
+import ThirdSection from './components/third-section';
+import CtaCard from './components/cta-card';
+
+const setOnboardedCookie = async () => {
+  'use server';
+  const cookieStore = cookies();
+  cookieStore.set('isOnboarded', 'true', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    maxAge: EXPIRED_COOKIE_ONE_YEAR
+  });
+};
+
+const OnboardingPage = () => {
+  return (
+    <section className="bg-onboarding-bg relative bg-[length:37.5rem_37.5rem] bg-[position:center_7.31rem] bg-no-repeat pt-8 text-center">
+      <FirstSection setCookie={setOnboardedCookie} />
+      <SecondSection />
+      <ThirdSection />
+      <CtaCard setCookie={setOnboardedCookie} />
+    </section>
+  );
+};
+
+export default OnboardingPage;
+
+const EXPIRED_COOKIE_ONE_YEAR = 60 * 60 * 24 * 365;
