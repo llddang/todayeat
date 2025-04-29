0528e53d24d0e32e2bd8def3ee5cbcc47f1fef9f
woohyuckk <woohyuckk@gmail.com>
2025-04-29 15:58:59 +0900

    chore:#244 문구 띄어쓰기 교정

--- src/app/(client)/meal/post/edit/constants/error-message.constant.ts
+++ src/app/(client)/meal/post/edit/constants/error-message.constant.ts
@@ -1,18 +1,18 @@
 export const ERROR_MESSAGES = {
   AI_ANALYSIS_FAILED: {
     title: 'AI 분석에 실패하였습니다.',
-    description: '메뉴명을 다시 입력해주세요.'
+    description: '메뉴명을 다시 입력해 주세요.'
   },
   MEAL_POST_FAILED: {
     title: ' 식사 정보 등록에 실패했습니다.',
-    description: '잠시 후 다시 시도해주세요.'
+    description: '잠시 후 다시 시도해 주세요.'
   },
   MEAL_DELETE_FAILED: {
     title: '식사 카드 삭제에 실패했습니다.',
-    description: '잠시 후 다시 시도해주세요.'
+    description: '잠시 후 다시 시도해 주세요.'
   },
   AI_ANALYSIS_FAILED_DEFAULT: {
     title: 'AI 분석 중 문제가 발생했습니다.',
-    description: '잠시 후 다시 시도해주세요.'
+    description: '잠시 후 다시 시도해 주세요.'
   }
 } as const;
