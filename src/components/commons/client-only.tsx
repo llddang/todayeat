e2e7ab811f9f2c0e7ef6aca752be5bb6996bc787
woohyuckk <woohyuckk@gmail.com>
2025-04-30 17:22:53 +0900

    refactor:#268  조건문 수정 및 주석 삭제

--- src/components/commons/client-only.tsx
+++ src/components/commons/client-only.tsx
@@ -3,11 +3,6 @@
 import { ComponentProps, ComponentType, ReactNode } from 'react';
 import useIsClient from '@/hooks/use-is-client';
 
-/**
- * ClientOnly 컴포넌트
- * @param {ReactNode} children 클라이언트에서 렌더링할 내용
- * @param {ReactNode} fallback 서버에서 렌더링할 내용
- */
 type ClientOnlyProps = {
   children: ReactNode;
   fallback: ReactNode;
