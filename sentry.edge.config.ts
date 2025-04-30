60ab6b7772f728bcdb5e96ca2c229a2c3c29da52
llddang <laliddang@gmail.com>
2025-05-01 00:52:38 +0900

    feat:#282 sentry error rate 0.2로 변경

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -11,7 +11,7 @@ Sentry.init({
   dsn: ENV.SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false
